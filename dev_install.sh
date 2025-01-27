932a0dad213ca59d76bbf44fe6b033414f509976
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2025-01-27 13:33:18 +0000

    Add a dev install

---
+++ dev_install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# controls_dev sets pip up to look at a local pypi server, which is incomplete
+module unload controls_dev 
+
+module load python/3.11
+
+if [ -d "./.venv" ]; then
+rm -rf .venv
+fi
+
+mkdir .venv
+
+python -m venv .venv
+source .venv/bin/activate
+
+pip install --upgrade pip
+pip install wheel
+pip install -e .[dev]
+
+pre-commit install
+
+# Local version of dodal
+if [ ! -d "../dodal" ]; then
+  git clone git@github.com:DiamondLightSource/dodal.git ../dodal
+fi
+
+pip install -e ../dodal[dev]
+
+pre-commit run --all-files
+pytest tests/
