b8b5852c24f9d813ae2b021730b8d23e447805c9
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2025-03-28 18:29:47 +0000

    Rename eh1 file and mirror for eh2

--- src/i19_bluesky/eh1/__init__.py
+++ src/i19_bluesky/eh1/__init__.py
@@ -1,6 +1,6 @@
 """A place for eh1 specific plans."""
 
-from .optics_hutch_control import close_experiment_shutter, open_experiment_shutter
+from .eh1_optics_hutch_control import close_experiment_shutter, open_experiment_shutter
 
 __all__ = [
     "open_experiment_shutter",
--- src/i19_bluesky/eh2/__init__.py
+++ src/i19_bluesky/eh2/__init__.py
@@ -1,1 +1,5 @@
 """A place for eh2 specific plans."""
+
+from .eh2_optics_hutch_control import close_experiment_shutter, open_experiment_shutter
+
+__all__ = ["open_experiment_shutter", "close_experiment_shutter"]
