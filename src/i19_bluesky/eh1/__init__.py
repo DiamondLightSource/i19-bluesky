aa7ba40b07dde24f644747b84b555621fa4e88cd
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2025-03-28 18:27:21 +0000

    Add entry points for eh1

--- src/i19_bluesky/eh1/__init__.py
+++ src/i19_bluesky/eh1/__init__.py
@@ -1,1 +1,8 @@
 """A place for eh1 specific plans."""
+
+from .optics_hutch_control import close_experiment_shutter, open_experiment_shutter
+
+__all__ = [
+    "open_experiment_shutter",
+    "close_experiment_shutter",
+]
