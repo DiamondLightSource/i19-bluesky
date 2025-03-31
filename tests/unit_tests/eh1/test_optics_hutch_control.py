047923444ec3c61ec324a34812106af3ee3829d4
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2025-03-31 10:47:51 +0100

    Add some tests

---
+++ tests/unit_tests/eh1/test_optics_hutch_control.py
@@ -0,0 +1,28 @@
+from unittest.mock import MagicMock, patch
+
+from bluesky.run_engine import RunEngine
+from dodal.devices.hutch_shutter import ShutterDemand
+from dodal.devices.i19.shutter import AccessControlledShutter
+
+from i19_bluesky.eh1.eh1_optics_hutch_control import (
+    close_experiment_shutter,
+    open_experiment_shutter,
+)
+
+
+@patch("i19_bluesky.eh1.eh1_optics_hutch_control.bps.abs_set")
+async def test_open_shutter_plan(
+    mock_set: MagicMock, eh1_shutter: AccessControlledShutter, RE: RunEngine
+):
+    RE(open_experiment_shutter(eh1_shutter))
+
+    mock_set.assert_called_once_with(eh1_shutter, ShutterDemand.OPEN, wait=True)
+
+
+@patch("i19_bluesky.eh1.eh1_optics_hutch_control.bps.abs_set")
+async def test_close_shutter_plan(
+    mock_set: MagicMock, eh1_shutter: AccessControlledShutter, RE: RunEngine
+):
+    RE(close_experiment_shutter(eh1_shutter))
+
+    mock_set.assert_called_once_with(eh1_shutter, ShutterDemand.CLOSE, wait=True)
