e18c489340b4fbe8304cfa269f520e39d715a945
Noemi <noemi.frisina@diamond.ac.uk>
2025-05-09 16:26:39 +0100

    Move system tests from dodal

---
+++ tests/system_tests/blueapi_system/example_devices_and_plans.py
@@ -0,0 +1,68 @@
+from enum import StrEnum
+
+import bluesky.plan_stubs as bps
+from blueapi.core import MsgGenerator
+from bluesky.protocols import Movable
+from dodal.common import inject
+from dodal.devices.i19.blueapi_device import HutchState, OpticsBlueAPIDevice
+from ophyd_async.core import AsyncStatus, StandardReadable
+from ophyd_async.sim import SimMotor
+
+
+class MotorPosition(StrEnum):
+    IN = "IN"
+    OUT = "OUT"
+
+
+class AccessControlledOpticsMotors(OpticsBlueAPIDevice):
+    def __init__(self, name: str = "") -> None:
+        self.hutch = HutchState.EH2
+        super().__init__(name)
+
+    @AsyncStatus.wrap
+    async def set(self, value: MotorPosition):
+        request_params = {
+            "name": "move_motors",
+            "params": {
+                "experiment_hutch": self.hutch.value,
+                "access_device": "access_control",
+                "position": value.value,
+            },
+        }
+        await super().set(request_params)
+
+
+class FakeOpticsMotors(StandardReadable, Movable[MotorPosition]):
+    def __init__(self, name: str = "") -> None:
+        self.motor1 = SimMotor("m1")
+        self.motor2 = SimMotor("m2")
+        super().__init__(name)
+
+    @AsyncStatus.wrap
+    async def set(self, value: MotorPosition):
+        if value == MotorPosition.IN:
+            await self.motor1.set(1.0)
+            await self.motor2.set(1.8)
+        else:
+            await self.motor1.set(0.0)
+            await self.motor2.set(0.0)
+
+
+async def optics_motors(name="optics_motors") -> FakeOpticsMotors:
+    motors = FakeOpticsMotors(name=name)
+    await motors.connect()
+    return motors
+
+
+MOTOR: FakeOpticsMotors = inject("optics_motors")
+
+
+def move_motors(
+    position: MotorPosition, motors: FakeOpticsMotors = MOTOR
+) -> MsgGenerator:
+    yield from bps.abs_set(motors, position, wait=True)
+
+
+# NOTE. Yeah, without access control I can't do a thing
+# Need one fixture for access allowed and one for access denied
+# And some way to decorate the plan? r maybe a different plan?
---
+++ tests/system_tests/test_blueapi_device_system.py
@@ -0,0 +1,43 @@
+"""System test for blueapi backed device used for access control on i19.
+
+HOWTO:
+- Step 1: Start blueapi server with no stompo configuration, which mirrors
+   the optics blueapi running on i19
+       blueapi -c no_stomp_config.yaml serve
+- Step 2: Run system tests
+       tox -e system-test
+"""
+
+import time
+
+import pytest
+from blueapi.client.client import BlueapiClient
+from blueapi.config import ApplicationConfig
+
+from .blueapi_system.example_devices_and_plans import AccessControlledOpticsMotors
+
+
+@pytest.fixture(scope="module", autouse=True)
+def wait_for_server():
+    client = BlueapiClient.from_config(config=ApplicationConfig())
+
+    for _ in range(20):
+        try:
+            client.get_environment()
+            return
+        except ConnectionError:
+            ...
+        time.sleep(0.5)
+    raise TimeoutError("No connection to blueapi server")
+
+
+@pytest.fixture
+def blueapi_client() -> BlueapiClient:
+    return BlueapiClient.from_config(config=ApplicationConfig())
+
+
+@pytest.fixture
+async def motors_with_blueapi() -> AccessControlledOpticsMotors:
+    ac_motors = AccessControlledOpticsMotors(name="motors_with_blueapi")
+    await ac_motors.connect()
+    return ac_motors
