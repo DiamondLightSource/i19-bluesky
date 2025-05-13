3ccab72081cb02f0840946e171f2a238047242df
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2025-05-13 14:55:13 +0100

    Add system test to CI

--- tests/system_tests/test_blueapi_device_system.py
+++ tests/system_tests/test_blueapi_device_system.py
@@ -41,6 +41,7 @@ def blueapi_client() -> BlueapiClient:
 @pytest.fixture
 async def motors_with_blueapi() -> AccessControlledOpticsMotors:
     ac_motors = AccessControlledOpticsMotors(name="motors_with_blueapi")
+    ac_motors.url = "https://localhost:8080"
     await ac_motors.connect()
     return ac_motors
 
