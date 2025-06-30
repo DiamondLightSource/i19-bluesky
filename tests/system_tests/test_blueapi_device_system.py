9dd66d848bb36636ebf0f6ac83904e7f2b846ce7
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2025-06-30 17:14:23 +0100

    Now devices

--- tests/system_tests/test_blueapi_device_system.py
+++ tests/system_tests/test_blueapi_device_system.py
@@ -14,12 +14,14 @@ from pathlib import Path
 import pytest
 from blueapi.client.client import BlueapiClient
 from blueapi.config import ApplicationConfig, RestConfig
-from blueapi.service.model import PlanResponse
+from blueapi.service.model import DeviceResponse, PlanResponse
 from pydantic import HttpUrl, TypeAdapter
 from requests.exceptions import ConnectionError
 
 from .blueapi_system.example_devices_and_plans import (
-    AccessControlledOpticsMotors,  # , move_motors
+    AccessControlledOpticsMotors,
+    # FakeOpticsMotors,
+    # MotorPosition,  # , move_motors
 )
 
 CONFIG = ApplicationConfig(api=RestConfig(url=HttpUrl("http://localhost:12345")))
@@ -44,10 +46,17 @@ def wait_for_server():
     raise TimeoutError("No connection to blueapi server")
 
 
+@pytest.fixture(autouse=True)
+def clean_existing_tasks(blueapi_client: BlueapiClient):
+    for task in blueapi_client.get_all_tasks().tasks:
+        blueapi_client.clear_task(task.task_id)
+    yield
+
+
 @pytest.fixture
 async def eh2_motors_with_blueapi() -> AccessControlledOpticsMotors:
     ac_motors = AccessControlledOpticsMotors(name="motors_with_blueapi")
-    ac_motors.url = "https://testhost:12345"
+    ac_motors.url = "https://localhost:12345"
     await ac_motors.connect()
     return ac_motors
 
@@ -55,8 +64,8 @@ async def eh2_motors_with_blueapi() -> AccessControlledOpticsMotors:
 # @pytest.mark.system_test
 # async def test_move_motors_plan_does_not_run_when_check_access_fails(
 #     eh2_motors_with_blueapi: AccessControlledOpticsMotors,
-#     client: BlueapiClient,
-#     # MOTOR: FakeOpticsMotors,
+#     blueapi_client: BlueapiClient,
+#     MOTOR: FakeOpticsMotors,
 # ):
 #     assert await MOTOR.motor1.user_readback.get_value() == 0.0
 #     assert await MOTOR.motor2.user_readback.get_value() == 0.0
@@ -81,6 +90,21 @@ def expected_plans() -> PlanResponse:
 def test_get_plans_by_name(blueapi_client: BlueapiClient, expected_plans: PlanResponse):
     for plan in expected_plans.plans:
         assert blueapi_client.get_plan(plan.name) == plan
+
+
+@pytest.fixture
+def expected_devices() -> DeviceResponse:
+    return TypeAdapter(DeviceResponse).validate_json(
+        (_DATA_PATH / "devices.json").read_text()
+    )
+
+
+def test_get_devices(blueapi_client: BlueapiClient, expected_devices: DeviceResponse):
+    retrieved_devices = blueapi_client.get_devices()
+    retrieved_devices.devices.sort(key=lambda x: x.name)
+    expected_devices.devices.sort(key=lambda x: x.name)
+
+    assert retrieved_devices == expected_devices
 
 
 # @pytest.mark.system_test
