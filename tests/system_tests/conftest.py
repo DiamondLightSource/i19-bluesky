fc8fe2eb5ee18d1c6c9b0a33c88b30180c33cd6e
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2025-07-04 14:43:31 +0100

    Maybe with a runengine

--- tests/system_tests/conftest.py
+++ tests/system_tests/conftest.py
@@ -1,4 +1,8 @@
+import asyncio
+import time
+
 import pytest
+from bluesky.run_engine import RunEngine
 from dodal.devices.i19.blueapi_device import HutchState
 from ophyd_async.testing import set_mock_value
 
@@ -10,7 +14,20 @@ from .blueapi_system.example_devices import (
 
 
 @pytest.fixture
-async def sim_motors() -> FakeOpticsMotors:
+async def RE():
+    RE = RunEngine({}, call_returns_result=True)
+    # make sure the event loop is thoroughly up and running before we try to create
+    # any ophyd_async devices which might need it
+    timeout = time.monotonic() + 1
+    while not RE.loop.is_running():
+        await asyncio.sleep(0)
+        if time.monotonic() > timeout:
+            raise TimeoutError("This really shouldn't happen but just in case...")
+    yield RE
+
+
+@pytest.fixture
+async def sim_motors(RE) -> FakeOpticsMotors:
     sim_motors = optics_motors(connect_immediately=True)
     set_mock_value(sim_motors.motor1.user_setpoint, 0.0)
     set_mock_value(sim_motors.motor2.user_setpoint, 0.0)
@@ -18,7 +35,7 @@ async def sim_motors() -> FakeOpticsMotors:
 
 
 @pytest.fixture
-async def eh2_motors_with_blueapi() -> AccessControlledOpticsMotors:
+async def eh2_motors_with_blueapi(RE) -> AccessControlledOpticsMotors:
     ac_motors = AccessControlledOpticsMotors(name="motors_with_blueapi")
     ac_motors.url = "https://localhost:12345"
     await ac_motors.connect()
@@ -26,7 +43,7 @@ async def eh2_motors_with_blueapi() -> AccessControlledOpticsMotors:
 
 
 @pytest.fixture
-async def eh1_motors_with_blueapi() -> AccessControlledOpticsMotors:
+async def eh1_motors_with_blueapi(RE) -> AccessControlledOpticsMotors:
     ac_motors = AccessControlledOpticsMotors(
         hutch=HutchState.EH1, name="motors_with_blueapi"
     )
