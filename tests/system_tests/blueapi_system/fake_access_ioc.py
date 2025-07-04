f7eb8dd492ec7e666d37710002376c3a3f680a36
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2025-07-04 13:00:43 +0100

    Try adding a softIOc to CI

---
+++ tests/system_tests/blueapi_system/fake_access_ioc.py
@@ -0,0 +1,35 @@
+# Import the basic framework components.
+# import asyncio
+
+from softioc import asyncio_dispatcher, builder, softioc
+
+if __name__ == "__main__":
+    # Create an asyncio dispatcher, the event loop is now running
+    dispatcher = asyncio_dispatcher.AsyncioDispatcher()
+
+    # Set the record prefix
+    builder.SetDeviceName("MOCK-ACCESS-CONTROL")
+
+    # Create some records
+    ai = builder.aIn("EHStatus.VALB", initial_value="EH1")
+    ao = builder.aOut(
+        "EHstatus.VALA",
+        initial_value="EH1",
+        always_update=True,
+        on_update=lambda v: ai.set(v),
+    )
+
+    # Boilerplate get the IOC started
+    builder.LoadDatabase()
+    softioc.iocInit(dispatcher)
+
+    # # Start processes required to be run after iocInit
+    # async def update():
+    #     while True:
+    #         ai.set(ai.get() + 1)
+    #         await asyncio.sleep(1)
+
+    # dispatcher(update)
+
+    # Finally leave the IOC running with an interactive shell.
+    softioc.interactive_ioc(globals())
