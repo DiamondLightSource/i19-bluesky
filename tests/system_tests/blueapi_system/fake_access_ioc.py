1ef05838bee5ba3d6592bf806282f482b3b941d2
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2025-07-04 16:07:55 +0100

    Still not working but remove comments

--- tests/system_tests/blueapi_system/fake_access_ioc.py
+++ tests/system_tests/blueapi_system/fake_access_ioc.py
@@ -10,12 +10,10 @@ if __name__ == "__main__":
     builder.SetDeviceName("MOCK-ACCESS-CONTROL")
 
     # Create some records
-    # ai = builder.stringIn("EHStatus.VALB", initial_value="EH1")
     ao = builder.stringOut(
         "EHStatus",
         initial_value="EH1",
         always_update=True,
-        # on_update=lambda v: ai.set(v),
     )
 
     # Boilerplate get the IOC started
