efd4ad3a8caff6a3e9bf35401b420d7c441ab1cf
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2025-07-07 13:57:38 +0100

    Update instructions

--- tests/system_tests/test_blueapi_device_system.py
+++ tests/system_tests/test_blueapi_device_system.py
@@ -1,11 +1,14 @@
 """System test for blueapi backed device used for access control on i19.
 
 HOWTO:
-- Step 1: Start blueapi server with no stompo configuration, which mirrors
+- Step 1: Start softIOC simulating the hutch access device PVs
+        python tests/system_tests/blueapi_system/fake_access_ioc.py
+- Step 2: Start blueapi server with no stompo configuration, which mirrors
    the optics blueapi running on i19
-       blueapi -c no_stomp_config.yaml serve
-- Step 2: Run system tests
+       blueapi -c tests/system_tests/blueapi_system/config_optics_blueapi.yaml serve
+- Step 3: Run system tests
        tox -e system-test
+       (or pytest tests/system_tests)
 """
 
 import time
