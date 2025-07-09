efe3db09d9b51729d8a3b3c1dd133342b999fd5a
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2025-07-09 18:46:39 +0100

    Try it

--- src/i19_bluesky/eh1/__init__.py
+++ src/i19_bluesky/eh1/__init__.py
@@ -1,5 +1,6 @@
 """A place for eh1 specific plans."""
 
+from i19_bluesky.optics import operate_shutter_plan
 from i19_bluesky.plans.optics_hutch_control_plans import (
     close_experiment_shutter,
     open_experiment_shutter,
@@ -7,5 +8,6 @@ from i19_bluesky.plans.optics_hutch_control_plans import (
 
 __all__ = [
     "open_experiment_shutter",
+    "operate_shutter_plan",
     "close_experiment_shutter",
 ]
