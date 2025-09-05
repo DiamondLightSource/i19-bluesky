44cc11efa705cf7289a4596ca59458e3515d47ed
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2025-09-05 18:07:11 +0100

    Add some logging

--- src/i19_bluesky/eh2/pincol_control_plans.py
+++ src/i19_bluesky/eh2/pincol_control_plans.py
@@ -2,6 +2,8 @@ import bluesky.plan_stubs as bps
 from bluesky.utils import MsgGenerator
 from dodal.devices.i19.pin_col_stages import PinColControl
 
+from i19_bluesky.log import LOGGER
+
 
 def move_pin_col_out_of_beam(
     pincol: PinColControl, group: str = "pincol_out", wait: bool = True
@@ -10,7 +12,9 @@ def move_pin_col_out_of_beam(
     colx_out = yield from bps.rd(pincol.config.col_x_out)
 
     # TODO check is move can be used, as long as it moves motors in the right order
+    LOGGER.info(f"Moving colx to out position {colx_out}")
     yield from bps.mv(pincol.collimator.x, colx_out, group=group)
+    LOGGER.info(f"Moving pinx to out position {pinx_out}")
     yield from bps.mv(pincol.pinhole.x, pinx_out, group=group)
 
     if wait:
