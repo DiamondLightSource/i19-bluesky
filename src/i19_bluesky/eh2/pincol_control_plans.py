1c0ae43a4d6e829c530784ef4994a4fd35cdc461
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2025-09-12 18:07:30 +0100

    Two basic tests -TBC

--- src/i19_bluesky/eh2/pincol_control_plans.py
+++ src/i19_bluesky/eh2/pincol_control_plans.py
@@ -1,5 +1,6 @@
 import bluesky.plan_stubs as bps
 from bluesky.utils import MsgGenerator
+from dodal.common import inject
 from dodal.devices.i19.pin_col_stages import (
     PinColRequest,
     PinholeCollimatorControl,
@@ -9,17 +10,17 @@ from i19_bluesky.log import LOGGER
 
 
 def move_pin_col_out_of_beam(
-    pincol: PinholeCollimatorControl, group: str = "pincol_out", wait: bool = True
+    pincol: PinholeCollimatorControl = inject("pincol"),  # noqa: B008
 ) -> MsgGenerator:
     LOGGER.info("Moving Pinhole and Collimator stages out")
-    yield from bps.abs_set(pincol, "OUT")
+    yield from bps.abs_set(pincol, "OUT", wait=True)
 
 
 def move_pin_col_to_requested_in_position(
-    pincol: PinholeCollimatorControl,
     aperture: PinColRequest,
+    pincol: PinholeCollimatorControl = inject("pincol"),  # noqa: B008
 ) -> MsgGenerator:
     LOGGER.info(
         f"Moving Pinhole and Collimator stages in to aperture: {aperture.value}"
     )
-    yield from bps.abs_set(pincol, aperture)
+    yield from bps.abs_set(pincol, aperture, wait=True)
