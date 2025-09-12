73f3542fc9f38e04e4acbb6523a7684707b618d1
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2025-09-12 17:57:01 +0100

    Update to latest dodal with all the logic in the device

--- src/i19_bluesky/eh2/pincol_control_plans.py
+++ src/i19_bluesky/eh2/pincol_control_plans.py
@@ -1,61 +1,25 @@
 import bluesky.plan_stubs as bps
 from bluesky.utils import MsgGenerator
 from dodal.devices.i19.pin_col_stages import (
-    CollimatorStage,
-    PinColControl,
     PinColRequest,
-    PinholeStage,
+    PinholeCollimatorControl,
 )
 
 from i19_bluesky.log import LOGGER
 
 
 def move_pin_col_out_of_beam(
-    pincol: PinColControl, group: str = "pincol_out", wait: bool = True
+    pincol: PinholeCollimatorControl, group: str = "pincol_out", wait: bool = True
 ) -> MsgGenerator:
-    pinx_out = yield from bps.rd(pincol.config.pin_x_out)
-    colx_out = yield from bps.rd(pincol.config.col_x_out)
-
-    # TODO check is move can be used, as long as it moves motors in the right order
-    LOGGER.info(f"Moving colx to out position {colx_out}")
-    yield from bps.mv(pincol.collimator.x, colx_out, group=group)
-    LOGGER.info(f"Moving pinx to out position {pinx_out}")
-    yield from bps.mv(pincol.pinhole.x, pinx_out, group=group)
-
-    if wait:
-        yield from bps.wait(group=group)
-
-
-def move_pinhole_motors_to_in_position(
-    pinhole: PinholeStage,
-    request: dict[str, float],
-) -> MsgGenerator:
-    LOGGER.info(f"Moving pinhole stage to positions {request}")
-    yield from bps.abs_set(pinhole, request)
-
-
-def move_collimator_motors_to_out_position(
-    collimator: CollimatorStage, request: dict[str, float]
-) -> MsgGenerator:
-    LOGGER.info(f"Move collimator stage to positions {request}")
-    yield from bps.abs_set(collimator, request)
+    LOGGER.info("Moving Pinhole and Collimator stages out")
+    yield from bps.abs_set(pincol, "OUT")
 
 
 def move_pin_col_to_requested_in_position(
-    pincol: PinColControl,
-    requested_aperture: PinColRequest,
+    pincol: PinholeCollimatorControl,
+    aperture: PinColRequest,
 ) -> MsgGenerator:
-    if requested_aperture in [PinColRequest.PINOUT, PinColRequest.COLOUT]:
-        raise ValueError(
-            """To move the pinhole and colliator stages out.
-            Please use the `move_pin_col_out_of_beam` plan."""
-        )
-    pinhole_pos = yield from pincol.get_pinhole_motor_positions_for_requested_aperture(
-        requested_aperture
+    LOGGER.info(
+        f"Moving Pinhole and Collimator stages in to aperture: {aperture.value}"
     )
-    collimator_pos = yield from pincol.get_collimator_motor_positions_for_requested_aperture(  # noqa E501
-        requested_aperture
-    )
-
-    yield from move_pinhole_motors_to_in_position(pincol.pinhole, pinhole_pos)
-    yield from move_collimator_motors_to_out_position(pincol.collimator, collimator_pos)
+    yield from bps.abs_set(pincol, aperture)
