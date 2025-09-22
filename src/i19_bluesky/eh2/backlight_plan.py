ac4bf5ee7fd395cb2a63961d5fab3cd441646feb
Shreelakshmi Iyengar <shreelakshmi.iyengar@diamond.ac.uk>
2025-09-22 11:35:59 +0100

    change name of plan to be more specific and add test that goes through plan and checks backlight position is in the right place

--- src/i19_bluesky/eh2/backlight_plan.py
+++ src/i19_bluesky/eh2/backlight_plan.py
@@ -7,14 +7,14 @@ from dodal.devices.i19.backlight import BacklightPosition
 from i19_bluesky.log import LOGGER
 
 
-def backlight_in(
+def move_backlight_in(
     backlight: BacklightPosition = inject("backlight"),  # noqa: B008
 ) -> MsgGenerator[None]:
     LOGGER.debug("Send command to move the backlight in.")
     yield from bps.abs_set(backlight, InOutUpper.IN, wait=True)
 
 
-def backlight_out(
+def move_backlight_out(
     backlight: BacklightPosition = inject("backlight"),  # noqa: B008
 ) -> MsgGenerator[None]:
     LOGGER.debug("Send command to move the backlight out.")
--- tests/unit_tests/eh2/test_backlight_plan.py
+++ tests/unit_tests/eh2/test_backlight_plan.py
@@ -1,12 +1,13 @@
 from unittest.mock import MagicMock, patch
 
+import pytest
 from bluesky.run_engine import RunEngine
 from dodal.common.enums import InOutUpper
 from dodal.devices.i19.backlight import BacklightPosition
 
 from i19_bluesky.eh2.backlight_plan import (
-    backlight_in,
-    backlight_out,
+    move_backlight_in,
+    move_backlight_out,
 )
 
 
@@ -14,7 +15,7 @@ from i19_bluesky.eh2.backlight_plan import (
 async def test_backlight_in_plan(
     mock_set: MagicMock, eh2_backlight: BacklightPosition, RE: RunEngine
 ):
-    RE(backlight_in(eh2_backlight))
+    RE(move_backlight_in(eh2_backlight))
 
     mock_set.assert_called_once_with(eh2_backlight, InOutUpper.IN, wait=True)
 
@@ -23,6 +24,18 @@ async def test_backlight_in_plan(
 async def test_backlight_out_plan(
     mock_set: MagicMock, eh2_backlight: BacklightPosition, RE: RunEngine
 ):
-    RE(backlight_out(eh2_backlight))
+    RE(move_backlight_out(eh2_backlight))
 
     mock_set.assert_called_once_with(eh2_backlight, InOutUpper.OUT, wait=True)
+
+
+@pytest.mark.parametrize(
+    "position, specific_plan",
+    [(InOutUpper.IN, move_backlight_in), (InOutUpper.OUT, move_backlight_out)],
+)
+async def test_backlight_position_plan(
+    eh2_backlight: BacklightPosition, RE: RunEngine, position, specific_plan
+):
+    RE(specific_plan(eh2_backlight))
+    await eh2_backlight.set(position)
+    assert await eh2_backlight.position.get_value() == position
