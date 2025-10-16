0f1493d414d1d742c3a4de7d820df67c2aa814ae
Shreelakshmi Iyengar <shreelakshmi.iyengar@diamond.ac.uk>
2025-10-16 17:17:17 +0100

    test: add test for panda seq table

---
+++ tests/unit_tests/serial/test_setup_panda.py
@@ -0,0 +1,15 @@
+import pytest
+from ophyd_async.fastcs.panda import SeqTrigger
+
+from i19_bluesky.serial.setup_panda import generate_panda_seq_table
+
+
+async def test_tables_are_same():
+    inputs = (5.6, 6.7, 10, 5)
+    table_from_func = generate_panda_seq_table(*inputs)
+
+    assert table_from_func.trigger == [SeqTrigger.POSA_GT, SeqTrigger.POSA_LT]
+    assert table_from_func.repeats == pytest.approx([10, 10])
+    assert table_from_func.position == pytest.approx([5, 6])
+    assert table_from_func.time1 == pytest.approx([5, 5])
+    assert table_from_func.outa1 == pytest.approx([True, True])
