589c608d1ed29481a57e1bbc2e688dd69dcd129b
Shreelakshmi Iyengar <shreelakshmi.iyengar@diamond.ac.uk>
2025-10-21 15:55:17 +0100

    test: edit conftest file to add enable to PulseBlock in new PulseBlockWithEnable device

--- src/i19_bluesky/serial/setup_panda.py
+++ src/i19_bluesky/serial/setup_panda.py
@@ -7,11 +7,11 @@ from i19_bluesky.log import LOGGER
 
 def arm_panda(panda: HDFPanda) -> MsgGenerator[None]:
     LOGGER.debug("Send command to arm the PandA.")
-    yield from bps.abs_set(panda.seq[1].enable, PandaBitMux.ONE, wait=True)
-    yield from bps.abs_set(panda.pulse[1].enable, PandaBitMux.ONE, wait=True)
+    yield from bps.abs_set(panda.seq[1].enable, PandaBitMux.ONE, wait=True)  # type: ignore
+    yield from bps.abs_set(panda.pulse[1].enable, PandaBitMux.ONE, wait=True)  # type:ignore
 
 
 def disarm_panda(panda: HDFPanda) -> MsgGenerator[None]:
     LOGGER.debug("Send command to disarm the PandA.")
-    yield from bps.abs_set(panda.seq[1].enable, PandaBitMux.ZERO, wait=True)
-    yield from bps.abs_set(panda.pulse[1].enable, PandaBitMux.ZERO, wait=True)
+    yield from bps.abs_set(panda.seq[1].enable, PandaBitMux.ZERO, wait=True)  # type: ignore
+    yield from bps.abs_set(panda.pulse[1].enable, PandaBitMux.ZERO, wait=True)  # type: ignore
--- tests/unit_tests/serial/test_setup_panda.py
+++ tests/unit_tests/serial/test_setup_panda.py
@@ -1,5 +1,5 @@
 from bluesky.run_engine import RunEngine
-from ophyd_async.fastcs.panda import HDFPanda, PandaBitMux
+from ophyd_async.fastcs.panda import PandaBitMux
 
 from i19_bluesky.serial.setup_panda import (
     arm_panda,
@@ -7,7 +7,7 @@ from i19_bluesky.serial.setup_panda import (
 )
 
 
-async def test_panda_arm(panda: HDFPanda, RE: RunEngine):
+async def test_panda_arm(panda, RE: RunEngine):
     await panda.seq[1].enable.set(PandaBitMux.ZERO)
     await panda.pulse[1].enable.set(PandaBitMux.ZERO)
     RE(arm_panda(panda))
@@ -15,7 +15,7 @@ async def test_panda_arm(panda: HDFPanda, RE: RunEngine):
     assert await panda.pulse[1].enable.get_value() == PandaBitMux.ONE
 
 
-async def test_panda_disarm(panda: HDFPanda, RE: RunEngine):
+async def test_panda_disarm(panda, RE: RunEngine):
     await panda.seq[1].enable.set(PandaBitMux.ONE)
     await panda.pulse[1].enable.set(PandaBitMux.ONE)
     RE(disarm_panda(panda))
