deeb7a69cd6d148b8b7fef3113be466b31155a77
Shreelakshmi Iyengar <shreelakshmi.iyengar@diamond.ac.uk>
2025-10-24 16:17:44 +0100

    refactor: use set_mock_value instead of await ... set

--- tests/unit_tests/serial/test_setup_panda.py
+++ tests/unit_tests/serial/test_setup_panda.py
@@ -1,5 +1,6 @@
 from bluesky.run_engine import RunEngine
 from ophyd_async.fastcs.panda import PandaBitMux
+from ophyd_async.testing import set_mock_value
 
 from i19_bluesky.serial.setup_panda import (
     arm_panda,
@@ -8,16 +9,16 @@ from i19_bluesky.serial.setup_panda import (
 
 
 async def test_panda_arm(panda, RE: RunEngine):
-    await panda.seq[1].enable.set(PandaBitMux.ZERO)
-    await panda.pulse[1].enable.set(PandaBitMux.ZERO)
+    set_mock_value(panda.seq[1].enable, PandaBitMux.ZERO)
+    set_mock_value(panda.pulse[1].enable, PandaBitMux.ZERO)
     RE(arm_panda(panda))
     assert await panda.seq[1].enable.get_value() == PandaBitMux.ONE
     assert await panda.pulse[1].enable.get_value() == PandaBitMux.ONE
 
 
 async def test_panda_disarm(panda, RE: RunEngine):
-    await panda.seq[1].enable.set(PandaBitMux.ONE)
-    await panda.pulse[1].enable.set(PandaBitMux.ONE)
+    set_mock_value(panda.seq[1].enable, PandaBitMux.ONE)
+    set_mock_value(panda.pulse[1].enable, PandaBitMux.ONE)
     RE(disarm_panda(panda))
     assert await panda.seq[1].enable.get_value() == PandaBitMux.ZERO
     assert await panda.pulse[1].enable.get_value() == PandaBitMux.ZERO
