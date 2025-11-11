f5808bb54f4efb41cd9bc115633f6cef930cdb4c
Noemi <noemi.frisina@diamond.ac.uk>
2025-11-11 17:42:32 +0000

    Add visit parameters

--- src/i19_bluesky/parameters/components.py
+++ src/i19_bluesky/parameters/components.py
@@ -1,6 +1,7 @@
 from enum import StrEnum
+from pathlib import Path
 
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 
 
 class HutchName(StrEnum):
@@ -8,5 +9,19 @@ class HutchName(StrEnum):
     EH2 = "EH2"
 
 
-class VisitParams(BaseModel):
-    pass
+class VisitParameters(BaseModel):
+    hutch: HutchName
+    visit: Path
+    dataset: str
+    filename_prefix: str
+
+    @field_validator("visit", mode="before")
+    @classmethod
+    def _parse_visit(cls, visit: str | Path):
+        if isinstance(visit, str):
+            return Path(visit)
+        return visit
+
+    @property
+    def collection_directory(self) -> Path:
+        return self.visit / self.dataset
