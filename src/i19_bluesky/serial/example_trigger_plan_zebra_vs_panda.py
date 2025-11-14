2c0e17cb96dee49b516ff0b745f6e8d7ee677004
Shreelakshmi Iyengar <shreelakshmi.iyengar@diamond.ac.uk>
2025-11-14 11:30:50 +0000

    fix: sort out diffractometer argument

--- src/i19_bluesky/serial/example_trigger_plan_zebra_vs_panda.py
+++ src/i19_bluesky/serial/example_trigger_plan_zebra_vs_panda.py
@@ -18,47 +18,51 @@ def setup_diffractometer(
     phi_start: float,
     phi_steps: int,
     exposure_time: int,
-    phi_velocity: int,
     diffractometer: FourCircleDiffractometer,
 ) -> MsgGenerator:
     """Setup phi on the diffractometer"""
     yield from bps.abs_set(diffractometer.phi, phi_start)
     velocity = phi_steps / exposure_time
-    yield from bps.abs_set(phi_velocity, velocity)
+    yield from bps.abs_set(diffractometer.phi.velocity, velocity)
 
 
-# def_phi_ramp_start(phi_start, phi_ramp_start):
 # phi_ramp_start = phi_start - 1
 
 
-def setup_zebra_positive(zebra: Zebra, phi_end, phi_value):
-    yield from setup_diffractometer(0, 0, 0, 0, FourCircleDiffractometer)
+def setup_zebra_positive(
+    zebra: Zebra, phi_end, diffractometer: FourCircleDiffractometer
+):
+    yield from setup_diffractometer(0, 0, 0, diffractometer=diffractometer)
     yield from setup_zebra_for_collection(
         zebra, RotationDirection.POSITIVE, 0, 0, 0, "setup_zebra_for_collection", True
     )
     yield from setup_out_triggers(zebra, "setup_out_triggers", True)
     yield from setup_zebra_for_triggering(zebra, "setup_zebra_for_triggering")
     arm_zebra(zebra)
-    yield from bps.abs_set(phi_value, phi_end)
+    yield from bps.abs_set(diffractometer.phi, phi_end)
     disarm_zebra(zebra)
 
 
-def setup_zebra_negative(zebra: Zebra, phi_end, phi_value):
+def setup_zebra_negative(
+    zebra: Zebra, phi_end, diffractometer: FourCircleDiffractometer
+):
     yield from setup_zebra_for_collection(
         zebra, RotationDirection.NEGATIVE, 0, 0, 0, "setup_zebra_for_collection", True
     )
     yield from setup_out_triggers(zebra, "setup_out_triggers", True)
     yield from setup_zebra_for_triggering(zebra, "setup_zebra_for_triggering")
     arm_zebra(zebra)
-    yield from bps.abs_set(phi_value, phi_end)
+    yield from bps.abs_set(diffractometer.phi, phi_end)
     disarm_zebra(zebra)
 
 
-def trigger_panda(panda: HDFPanda, phi_value, phi_start, phi_end):
-    yield from setup_diffractometer(0, 0, 0, 0, FourCircleDiffractometer)
+def trigger_panda(
+    panda: HDFPanda, diffractometer: FourCircleDiffractometer, phi_start, phi_end
+):
+    yield from setup_diffractometer(0, 0, 0, diffractometer=diffractometer)
     yield from setup_panda_for_rotation(panda, 0, 0, 0, 0, 0)
     yield from arm_panda(panda)
-    yield from bps.abs_set(phi_value, phi_end)
+    yield from bps.abs_set(diffractometer.phi, phi_end)
     yield from bps.wait()
-    yield from bps.abs_set(phi_value, phi_start)
+    yield from bps.abs_set(diffractometer.phi, phi_start)
     yield from disarm_panda(panda)
