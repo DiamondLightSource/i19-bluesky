5cf32bada7ad82db921ae22c14259b4f9329e84d
Shreelakshmi Iyengar <shreelakshmi.iyengar@diamond.ac.uk>
2025-11-21 15:16:49 +0000

    feat: add functions created during testing

--- src/i19_bluesky/serial/example_trigger_plan_zebra_vs_panda.py
+++ src/i19_bluesky/serial/example_trigger_plan_zebra_vs_panda.py
@@ -1,10 +1,13 @@
 import bluesky.plan_stubs as bps
+import bluesky.preprocessors as bpp
 from bluesky.utils import MsgGenerator
+from dodal.common import inject
 from dodal.devices.i19.diffractometer import FourCircleDiffractometer
 from dodal.devices.zebra.zebra import RotationDirection, Zebra
 from ophyd_async.fastcs.panda import HDFPanda
 
 from i19_bluesky.eh2.zebra_arming_plan import arm_zebra, disarm_zebra
+from i19_bluesky.log import LOGGER
 from i19_bluesky.serial.panda_setup_plans import setup_panda_for_rotation
 from i19_bluesky.serial.panda_stubs import arm_panda, disarm_panda
 from i19_bluesky.serial.zebra_collection_setup_plan import setup_zebra_for_collection
@@ -13,7 +16,7 @@ from i19_bluesky.serial.zebra_collection_setup_plan import setup_zebra_for_colle
 def setup_diffractometer(
     ramp_value: float,
     phi_steps: int,
-    exposure_time: int,
+    exposure_time: float,
     diffractometer: FourCircleDiffractometer,
 ) -> MsgGenerator:
     """Setup phi on the diffractometer"""
@@ -27,7 +30,7 @@ def trigger_zebra(
     phi_start: float,
     phi_end: float,
     phi_steps: int,
-    exposure_time: int,
+    exposure_time: float,
     diffractometer: FourCircleDiffractometer,
     gate_width: float,
     pulse_width: float,
@@ -78,3 +81,46 @@ def trigger_panda(
     yield from bps.sleep(2.0)
     yield from bps.abs_set(diffractometer.phi, phi_start)
     yield from disarm_panda(panda)
+
+
+def abort_zebra(diffractometer: FourCircleDiffractometer, zebra: Zebra) -> MsgGenerator:
+    LOGGER.warning("I AM ABORTING")
+    yield from bps.abs_set(diffractometer.phi.motor_stop, 1, wait=True)
+    yield from disarm_zebra(zebra)
+
+
+def abort_panda(
+    diffractometer: FourCircleDiffractometer, panda: HDFPanda
+) -> MsgGenerator:
+    LOGGER.warning("I AM ABORTING")
+    yield from bps.abs_set(diffractometer.phi.motor_stop, 1, wait=True)
+    yield from disarm_panda(panda)
+
+
+def move_diffractometer_back(diffractometer: FourCircleDiffractometer) -> MsgGenerator:
+    LOGGER.info("Move diffractometer back to where it was")
+    yield from bps.abs_set(diffractometer.phi, 19, wait=True)
+
+
+def run_panda_test(
+    diffractometer: FourCircleDiffractometer = inject("diffractometer"),
+    panda: HDFPanda = inject("panda"),
+) -> MsgGenerator:
+    yield from bpp.contingency_wrapper(
+        trigger_panda(panda, diffractometer, 19.5, 20, 23, 30, 0.1),
+        except_plan=lambda: (yield from abort_panda(diffractometer, panda)),
+        final_plan=lambda: (yield from move_diffractometer_back(diffractometer)),
+        auto_raise=False,
+    )
+
+
+def run_zebra_test(
+    diffractometer: FourCircleDiffractometer = inject("diffractometer"),
+    zebra: Zebra = inject("zebra"),
+) -> MsgGenerator:
+    yield from bpp.contingency_wrapper(
+        trigger_zebra(zebra, 20, 23, 30, 0.1, diffractometer, 3.1, 1),
+        except_plan=lambda: (yield from abort_zebra(diffractometer, zebra)),
+        final_plan=lambda: (yield from move_diffractometer_back(diffractometer)),
+        auto_raise=False,
+    )
