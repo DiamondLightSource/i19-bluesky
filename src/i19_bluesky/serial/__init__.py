c77cf7cb7066d3e3eb93fb6c09d3a97a9c062349
Shreelakshmi Iyengar <shreelakshmi.iyengar@diamond.ac.uk>
2025-11-25 12:02:08 +0000

    feat: expose example trigger plan

--- src/i19_bluesky/serial/__init__.py
+++ src/i19_bluesky/serial/__init__.py
@@ -1,1 +1,23 @@
 """All things serial go here."""
+
+from i19_bluesky.serial.example_trigger_plan_zebra_vs_panda import (
+    abort_panda,
+    abort_zebra,
+    move_diffractometer_back,
+    run_panda_test,
+    run_zebra_test,
+    setup_diffractometer,
+    trigger_panda,
+    trigger_zebra,
+)
+
+__all__ = [
+    "setup_diffractometer",
+    "trigger_zebra",
+    "trigger_panda",
+    "abort_zebra",
+    "abort_panda",
+    "move_diffractometer_back",
+    "run_panda_test",
+    "run_zebra_test",
+]
