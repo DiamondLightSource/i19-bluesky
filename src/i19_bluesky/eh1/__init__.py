8e95910e9964df4636355c537cc03872a342c5fb
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2025-12-11 14:50:10 +0000

    Aaaand maybe add entry points to eh1 too...

--- src/i19_bluesky/eh1/__init__.py
+++ src/i19_bluesky/eh1/__init__.py
@@ -2,10 +2,18 @@
 
 from i19_bluesky.plans.optics_hutch_control_plans import (
     close_experiment_shutter,
+    nudge_hfm_piezo_actuators,
+    nudge_vfm_piezo_actuators,
     open_experiment_shutter,
+    set_requested_voltage_to_hfm_piezo,
+    set_requested_voltage_to_vfm_piezo,
 )
 
 __all__ = [
     "open_experiment_shutter",
     "close_experiment_shutter",
+    "nudge_hfm_piezo_actuators",
+    "nudge_vfm_piezo_actuators",
+    "set_requested_voltage_to_hfm_piezo",
+    "set_requested_voltage_to_vfm_piezo",
 ]
