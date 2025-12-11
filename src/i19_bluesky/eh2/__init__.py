6d7ff7cba57215cd73f995e1fc032e298873abd2
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2025-12-11 14:49:09 +0000

    Add entry point plans for piezo control

--- src/i19_bluesky/eh2/__init__.py
+++ src/i19_bluesky/eh2/__init__.py
@@ -6,7 +6,11 @@ from i19_bluesky.eh2.pincol_control_plans import (
 )
 from i19_bluesky.plans.optics_hutch_control_plans import (
     close_experiment_shutter,
+    nudge_hfm_piezo_actuators,
+    nudge_vfm_piezo_actuators,
     open_experiment_shutter,
+    set_requested_voltage_to_hfm_piezo,
+    set_requested_voltage_to_vfm_piezo,
 )
 
 __all__ = [
@@ -14,4 +18,8 @@ __all__ = [
     "close_experiment_shutter",
     "move_pin_col_out_of_beam",
     "move_pin_col_to_requested_in_position",
+    "nudge_hfm_piezo_actuators",
+    "nudge_vfm_piezo_actuators",
+    "set_requested_voltage_to_hfm_piezo",
+    "set_requested_voltage_to_vfm_piezo",
 ]
--- src/i19_bluesky/plans/optics_hutch_control_plans.py
+++ src/i19_bluesky/plans/optics_hutch_control_plans.py
@@ -5,20 +5,62 @@ import bluesky.plan_stubs as bps
 from bluesky.utils import MsgGenerator
 from dodal.common import inject
 from dodal.devices.hutch_shutter import ShutterDemand
+from dodal.devices.i19.access_controlled.piezo_control import (
+    AccessControlledPiezoActuator,
+)
 from dodal.devices.i19.access_controlled.shutter import AccessControlledShutter
 
 from i19_bluesky.log import LOGGER
 
 
 def open_experiment_shutter(
-    shutter: AccessControlledShutter = inject("shutter"),  # noqa: B008
+    shutter: AccessControlledShutter = inject("shutter"),
 ) -> MsgGenerator[None]:
     LOGGER.debug("Send command to open the shutter.")
     yield from bps.abs_set(shutter, ShutterDemand.OPEN, wait=True)
 
 
 def close_experiment_shutter(
-    shutter: AccessControlledShutter = inject("shutter"),  # noqa: B008
+    shutter: AccessControlledShutter = inject("shutter"),
 ) -> MsgGenerator[None]:
     LOGGER.debug("Send command to close the shutter.")
     yield from bps.abs_set(shutter, ShutterDemand.CLOSE, wait=True)
+
+
+def apply_voltage_to_piezo_actuators(
+    requested_voltage: float, piezo_device: AccessControlledPiezoActuator
+) -> MsgGenerator[None]:
+    LOGGER.info(f"Applying {requested_voltage} to {piezo_device.name}")
+    yield from bps.abs_set(piezo_device, requested_voltage, wait=True)
+
+
+def set_requested_voltage_to_hfm_piezo(
+    requested_voltage: float,
+    hfm_piezo: AccessControlledPiezoActuator = inject("hfm_piezo"),
+) -> MsgGenerator[None]:
+    yield from apply_voltage_to_piezo_actuators(requested_voltage, hfm_piezo)
+
+
+def set_requested_voltage_to_vfm_piezo(
+    requested_voltage: float,
+    vfm_piezo: AccessControlledPiezoActuator = inject("vfm_piezo"),
+) -> MsgGenerator[None]:
+    yield from apply_voltage_to_piezo_actuators(requested_voltage, vfm_piezo)
+
+
+def nudge_hfm_piezo_actuators(
+    delta: float, hfm_piezo: AccessControlledPiezoActuator = inject("hfm_piezo")
+) -> MsgGenerator[None]:
+    LOGGER.debug(f"Nudge hfm piezo by {delta} V.")
+    current_voltage = yield from bps.rd(hfm_piezo.readback)
+    requested_voltage = current_voltage + delta
+    yield from apply_voltage_to_piezo_actuators(requested_voltage, hfm_piezo)
+
+
+def nudge_vfm_piezo_actuators(
+    delta: float, vfm_piezo: AccessControlledPiezoActuator = inject("hfm_piezo")
+) -> MsgGenerator[None]:
+    LOGGER.debug(f"Nudge vfm piezo by {delta} V.")
+    current_voltage = yield from bps.rd(vfm_piezo.readback)
+    requested_voltage = current_voltage + delta
+    yield from apply_voltage_to_piezo_actuators(requested_voltage, vfm_piezo)
