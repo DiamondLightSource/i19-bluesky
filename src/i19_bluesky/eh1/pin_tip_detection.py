973c967da3c7b8bf8a724eb4fd5401fdcfc15fdf
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2026-01-26 11:37:39 +0000

    Update to latest dodal changes

--- src/i19_bluesky/eh1/pin_tip_detection.py
+++ src/i19_bluesky/eh1/pin_tip_detection.py
@@ -1,5 +1,5 @@
 import bluesky.plan_stubs as bps
-from dodal.devices.i19.pin_tip import PinTipCentre
+from dodal.devices.i19.pin_tip import PinTipCentreHolder
 from dodal.devices.oav.oav_parameters import OAVParameters
 from dodal.devices.oav.pin_image_recognition import PinTipDetection
 from dodal.plans.device_setup_plans import setup_pin_tip_detection_params
@@ -11,7 +11,7 @@ OAV_CONFIG_JSON = "/dls_sw/i19-1/software/bluesky/OAVCentring.json"
 def pin_tip_detection_plan(
     centring_context: str,
     pin_tip_detection: PinTipDetection,
-    beam_centre: PinTipCentre,
+    pin_tip_centre: PinTipCentreHolder,
     oav_config: str = OAV_CONFIG_JSON,
 ):
     oav_parameters = OAVParameters(centring_context, oav_config)
@@ -23,8 +23,8 @@ def pin_tip_detection_plan(
     tip_x_y_px = yield from bps.rd(pin_tip_detection.triggered_tip)
 
     yield from bps.abs_set(
-        beam_centre.beam_centre_i, int(tip_x_y_px[0]), group="set_beam_center"
+        pin_tip_centre.pin_tip_i, int(tip_x_y_px[0]), group="set_beam_center"
     )
     yield from bps.abs_set(
-        beam_centre.beam_centre_j, int(tip_x_y_px[1]), group="set_beam_center"
+        pin_tip_centre.pin_tip_j, int(tip_x_y_px[1]), group="set_beam_center"
     )
