7604ab47c0324c44f587ea1ab3cbcfd336a89fa3
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2026-02-02 14:30:00 +0000

    Fix conftest

--- tests/unit_tests/optics/conftest.py
+++ tests/unit_tests/optics/conftest.py
@@ -21,7 +21,7 @@ def active_device_factories() -> set[AnyDeviceFactory]:
 
 @pytest.fixture
 def expt_shutter(RE) -> HutchShutter:
-    expt_shutter = i19_optics.shutter(connect_immediately=True, mock=True)
+    expt_shutter = i19_optics.shutter.build(connect_immediately=True, mock=True)
     set_mock_value(expt_shutter.interlock.status, HUTCH_SAFE_FOR_OPERATIONS)
 
     def set_status(value: ShutterDemand, *args, **kwargs):
@@ -34,19 +34,21 @@ def expt_shutter(RE) -> HutchShutter:
 
 @pytest.fixture
 def access_control_device(RE) -> HutchAccessControl:
-    access_control = i19_optics.access_control(connect_immediately=True, mock=True)
+    access_control = i19_optics.access_control.build(
+        connect_immediately=True, mock=True
+    )
     return access_control
 
 
 @pytest.fixture
 def vfm(RE) -> FocusingMirrorWithPiezo:
-    vfm = i19_optics.vfm(connect_immediately=True, mock=True)
+    vfm = i19_optics.vfm.build(connect_immediately=True, mock=True)
     set_mock_value(vfm.piezo, 1.0)
     return vfm
 
 
 @pytest.fixture
 def hfm(RE) -> FocusingMirrorWithPiezo:
-    hfm = i19_optics.hfm(connect_immediately=True, mock=True)
+    hfm = i19_optics.hfm.build(connect_immediately=True, mock=True)
     set_mock_value(hfm.piezo, 3.0)
     return hfm
