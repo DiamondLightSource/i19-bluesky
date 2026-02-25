c0972697c499c5bad1db890f0517ea8e6d2fcedb
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2026-02-25 17:11:26 +0000

    Add a small dummy plan to find the beam centre

--- src/i19_bluesky/eh1/__init__.py
+++ src/i19_bluesky/eh1/__init__.py
@@ -1,5 +1,6 @@
 """A place for eh1 specific plans."""
 
+from i19_bluesky.eh1.find_beam_centre import find_beam_centre_plan
 from i19_bluesky.eh1.pin_tip_detection import pin_tip_detection_plan
 from i19_bluesky.plans.optics_hutch_control_plans import (
     apply_voltage_to_piezo_actuators,
@@ -12,4 +13,5 @@ __all__ = [
     "close_experiment_shutter",
     "apply_voltage_to_piezo_actuators",
     "pin_tip_detection_plan",
+    "find_beam_centre_plan",
 ]
