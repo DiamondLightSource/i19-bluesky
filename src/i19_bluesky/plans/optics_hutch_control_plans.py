6806d9942aafc3a96ea00638ea08cc4c99121132
Paul Coe <paul.coe@diamond.ac.uk>
2026-03-05 16:33:12 +0000

    Add EH plans for setting attenuator squad positions
    * Resolves #113
    
    * Motors for attenuator wedges and position of the filter wheel
      are demanded on an I19 EH-1 or EH-2 by calling these plans.
    
    * The validated demand is passed on via REST call to the I19 Optics
      bluesky service
    
    * Usual EH-1 and EH-2 plan unit tests with test fixture configuration

--- src/i19_bluesky/plans/optics_hutch_control_plans.py
+++ src/i19_bluesky/plans/optics_hutch_control_plans.py
@@ -4,6 +4,10 @@ from one of the hutches."""
 import bluesky.plan_stubs as bps
 from bluesky.utils import MsgGenerator
 from dodal.common import inject
+from dodal.devices.beamlines.i19.access_controlled.attenuator_motor_squad import (
+    AttenuatorMotorPositionDemands,
+    AttenuatorMotorSquad,
+)
 from dodal.devices.beamlines.i19.access_controlled.piezo_control import (
     AccessControlledPiezoActuator,
 )
@@ -13,6 +17,21 @@ from dodal.devices.beamlines.i19.access_controlled.shutter import (
 from dodal.devices.hutch_shutter import ShutterDemand
 
 from i19_bluesky.log import LOGGER
+
+
+def apply_attenuator_positions(
+    motor_squad: AttenuatorMotorSquad, position_demands: AttenuatorMotorPositionDemands
+) -> MsgGenerator[None]:
+    validated_demands = position_demands.validated_complete_demand()
+    LOGGER.info(f"Applying position demands {validated_demands} to attenuator elements")
+    yield from bps.abs_set(motor_squad, position_demands, wait=True)
+
+
+def apply_voltage_to_piezo_actuators(
+    requested_voltage: float, piezo_device: AccessControlledPiezoActuator
+) -> MsgGenerator[None]:
+    LOGGER.info(f"Applying {requested_voltage} to {piezo_device.name}")
+    yield from bps.abs_set(piezo_device, requested_voltage, wait=True)
 
 
 def open_experiment_shutter(
@@ -27,10 +46,3 @@ def close_experiment_shutter(
 ) -> MsgGenerator[None]:
     LOGGER.debug("Send command to close the shutter.")
     yield from bps.abs_set(shutter, ShutterDemand.CLOSE, wait=True)
-
-
-def apply_voltage_to_piezo_actuators(
-    requested_voltage: float, piezo_device: AccessControlledPiezoActuator
-) -> MsgGenerator[None]:
-    LOGGER.info(f"Applying {requested_voltage} to {piezo_device.name}")
-    yield from bps.abs_set(piezo_device, requested_voltage, wait=True)
