b2141220854c6fe3247615ef0ad68b21a35ba796
Matthew Carre <matthew.carre@diamond.ac.uk>
2026-03-10 10:50:42 +0000

    Finalised move detector stage plan + tests

--- tests/unit_tests/serial/test_move_detector_stage.py
+++ tests/unit_tests/serial/test_move_detector_stage.py
@@ -7,11 +7,7 @@ from dodal.devices.beamlines.i19.diffractometer import (
 )
 from ophyd_async.core import get_mock_put
 
-<<<<<<< HEAD
-from i19_bluesky.eh2.move_detector_stage import move_detector_stage
-=======
 from i19_bluesky.eh2.move_detector_stage import move_stage
->>>>>>> f9418c7 (Added tests for moving detector stage)
 
 
 @pytest.mark.parametrize(
@@ -23,15 +19,7 @@ async def test_move_detector_stage(
     eh2_diffractometer: FourCircleDiffractometer,
     RE: RunEngine,
 ):
-    RE(
-<<<<<<< HEAD
-        move_detector_stage(
-            eh2_diffractometer.det_stage, detector_z, detector_two_theta
-        )
-=======
-        move_stage(eh2_diffractometer.det_stage, detector_z, detector_two_theta)  # type: ignore
->>>>>>> f9418c7 (Added tests for moving detector stage)
-    )
+    RE(move_stage(eh2_diffractometer.det_stage, detector_z, detector_two_theta))
     assert (
         await eh2_diffractometer.det_stage.det_z.user_readback.get_value() == detector_z
     )
@@ -50,11 +38,7 @@ async def test_move_order_det_z_first(
     mock_two_theta = get_mock_put(eh2_diffractometer.det_stage.two_theta.user_setpoint)
     parent.attach_mock(mock_det_z, "det_z")
     parent.attach_mock(mock_two_theta, "two_theta")
-<<<<<<< HEAD
-    RE(move_detector_stage(eh2_diffractometer.det_stage, 120, 45))
-=======
     RE(move_stage(eh2_diffractometer.det_stage, 120, 45))
->>>>>>> f9418c7 (Added tests for moving detector stage)
     parent.assert_has_calls([call.det_z(120), call.two_theta(45)])
 
 
@@ -62,18 +46,11 @@ async def test_move_order_two_theta_first(
     eh2_diffractometer: FourCircleDiffractometer,
     RE: RunEngine,
 ):
-<<<<<<< HEAD
-=======
     RE(move_stage(eh2_diffractometer.det_stage, 80, 90))
->>>>>>> f9418c7 (Added tests for moving detector stage)
     parent = MagicMock()
     mock_det_z = get_mock_put(eh2_diffractometer.det_stage.det_z.user_setpoint)
     mock_two_theta = get_mock_put(eh2_diffractometer.det_stage.two_theta.user_setpoint)
     parent.attach_mock(mock_det_z, "det_z")
     parent.attach_mock(mock_two_theta, "two_theta")
     RE(move_stage(eh2_diffractometer.det_stage, 80, 90))
-<<<<<<< HEAD
-    RE(move_detector_stage(eh2_diffractometer.det_stage, 80, 90))
-=======
->>>>>>> f9418c7 (Added tests for moving detector stage)
     parent.assert_has_calls([call.two_theta(90), call.det_z(80)])
