ccd379f5baee863e5a489aadd794321ac864ec91
Matthew Carre <matthew.carre@diamond.ac.uk>
2026-03-11 10:00:45 +0000

    Added logging to detector setup

--- src/i19_bluesky/serial/setup_detector.py
+++ src/i19_bluesky/serial/setup_detector.py
@@ -1,3 +1,5 @@
+from bluesky.log import logger
+from bluesky.utils import MsgGenerator
 from dodal.devices.beamlines.i19.diffractometer import FourCircleDiffractometer
 
 import i19_bluesky.eh2.move_detector_stage as mvstgpln
@@ -5,7 +7,7 @@ import i19_bluesky.eh2.move_detector_stage as mvstgpln
 
 def setup_detector(
     diffractometer: FourCircleDiffractometer, det_z: float, two_theta: float
-):
+) -> MsgGenerator:
     """Runs setup tasks prior to data collection. Currently, moves the diffractometer\
         an inputted distance in the X and Two-Theta axis.
         Args:
@@ -15,4 +17,6 @@ def setup_detector(
             two_theta : Float
                 (default 0.0)
                 Distance to move in Two-Theta axis"""
+
+    logger.info("Moving detector stage into position")
     yield from mvstgpln.move_stage(diffractometer.det_stage, det_z, two_theta)
