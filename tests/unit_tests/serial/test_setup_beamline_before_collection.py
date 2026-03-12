c4d3938ace68c87c8f2413bbd69d0c734d92c27a
Matthew Carre <matthew.carre@diamond.ac.uk>
2026-03-12 11:09:48 +0000

    edited conftest.py

--- tests/unit_tests/serial/test_setup_beamline_before_collection.py
+++ tests/unit_tests/serial/test_setup_beamline_before_collection.py
@@ -1,7 +1,14 @@
+from unittest.mock import AsyncMock
+
 import pytest
 from bluesky.run_engine import RunEngine
+from dodal.devices.beamlines.i19.backlight import BacklightPosition
 from dodal.devices.beamlines.i19.diffractometer import (
     FourCircleDiffractometer,
+)
+from dodal.devices.beamlines.i19.pin_col_stages import (
+    PinColRequest,
+    PinholeCollimatorControl,
 )
 
 from i19_bluesky.serial.setup_beamline_pre_collection import (
@@ -12,15 +19,30 @@ from i19_bluesky.serial.setup_beamline_pre_collection import (
 @pytest.mark.parametrize(
     "detector_z,detector_two_theta", [(200, 0), (100, 30), (80, 90)]
 )
+@pytest.mark.parametrize(
+    "eh2_aperture",
+    [
+        (PinColRequest.PCOL20),
+        (PinColRequest.PCOL100),
+    ],
+)
 async def test_setup_beamline_before_collection(
     detector_z: float,
     detector_two_theta: float,
     eh2_diffractometer: FourCircleDiffractometer,
     RE: RunEngine,
+    eh2_backlight: BacklightPosition,
+    eh2_aperture: PinColRequest,
+    pincol: PinholeCollimatorControl,
 ):
     RE(
         setup_beamline_before_collection(
-            eh2_diffractometer, detector_z, detector_two_theta
+            eh2_backlight,
+            eh2_diffractometer,
+            pincol,
+            eh2_aperture,
+            detector_z,
+            detector_two_theta,
         )
     )
     assert (
@@ -30,3 +52,15 @@ async def test_setup_beamline_before_collection(
         await eh2_diffractometer.det_stage.two_theta.user_readback.get_value()
         == detector_two_theta
     )
+    mock_setup_beamline_before_collection = AsyncMock()
+    RE(
+        mock_setup_beamline_before_collection(
+            eh2_backlight,
+            eh2_diffractometer,
+            pincol,
+            eh2_aperture,
+            detector_z,
+            detector_two_theta,
+        )
+    )
+    mock_setup_beamline_before_collection.assert_called_once()
