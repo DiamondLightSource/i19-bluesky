5877b3f61a5c74a2f4106e92832e27af5b378ab5
Matthew Carre <matthew.carre@diamond.ac.uk>
2026-03-16 12:19:26 +0000

    Altered tests, UV.lock

--- tests/unit_tests/serial/test_run_serial_with_panda.py
+++ tests/unit_tests/serial/test_run_serial_with_panda.py
@@ -15,6 +15,7 @@ from ophyd_async.fastcs.panda import HDFPanda
 
 from i19_bluesky.serial.run_serial_with_panda import (
     abort_panda,
+    main_entry_plan,
     move_diffractometer_back,
     setup_diffractometer,
     setup_then_trigger_panda,
@@ -24,7 +25,11 @@ from i19_bluesky.serial.run_serial_with_panda import (
 
 @pytest.mark.parametrize(
     "detector_z,detector_two_theta,phi_start,phi_end,phi_steps,exposure_time",
-    [(100, 30, 50, 60, 0.5, 0.2)],
+    [
+        (100, 30, 50, 60, 0.5, 0.2),
+        (80, 90, 50, 60, 0.5, 0.2),
+        # (301, 90, 50, 60, 0.5, 0.2),
+    ],
 )
 @pytest.mark.parametrize(
     "eh2_aperture",
@@ -33,7 +38,13 @@ from i19_bluesky.serial.run_serial_with_panda import (
         (PinColRequest.PCOL100),
     ],
 )
+@patch("i19_bluesky.serial.run_serial_with_panda.move_diffractometer_back")
+@patch("i19_bluesky.serial.run_serial_with_panda.setup_then_trigger_panda")
+@patch("i19_bluesky.serial.run_serial_with_panda.abort_panda")
 async def test_main_entry_plan(
+    mock_abort_panda: MagicMock,
+    mock_setup_then_trigger_panda: MagicMock,
+    mock_move_diffractometer_back: MagicMock,
     detector_z: float,
     detector_two_theta: float,
     phi_start: float,
@@ -47,38 +58,64 @@ async def test_main_entry_plan(
     pincol: PinholeCollimatorControl,
     mock_panda: HDFPanda,
 ):
-    mock_main_entry_plan = MagicMock()
-    RE(
-        mock_main_entry_plan(
-            detector_z,
-            detector_two_theta,
-            phi_start,
-            phi_end,
-            phi_steps,
-            exposure_time,
-            eh2_aperture,
-            eh2_diffractometer,
-            eh2_backlight,
-            pincol,
-            mock_panda,
+    # assert that abort panda is ran if required
+    # if detector_z > 300:
+    #    raise ValueError
+    # This am unsure how to impliment, am looking up ways to trigger wrapper.
+    if detector_z > 300:
+        with pytest.raises(ValueError):
+            RE(
+                main_entry_plan(
+                    detector_z,
+                    detector_two_theta,
+                    phi_start,
+                    phi_end,
+                    phi_steps,
+                    exposure_time,
+                    eh2_aperture,
+                    eh2_diffractometer,
+                    eh2_backlight,
+                    pincol,
+                    mock_panda,
+                )
+            )
+            mock_setup_then_trigger_panda.assert_called_once()
+            mock_abort_panda.assert_called_once()
+            mock_move_diffractometer_back.assert_called_once_with(
+                eh2_diffractometer, phi_start
+            )
+    else:
+        RE(
+            main_entry_plan(
+                detector_z,
+                detector_two_theta,
+                phi_start,
+                phi_end,
+                phi_steps,
+                exposure_time,
+                eh2_aperture,
+                eh2_diffractometer,
+                eh2_backlight,
+                pincol,
+                mock_panda,
+            )
         )
-    )
-    mock_main_entry_plan.assert_called_once()
+    mock_setup_then_trigger_panda.assert_called_once()
+    mock_move_diffractometer_back.assert_called_once_with(eh2_diffractometer, phi_start)
 
 
 @pytest.mark.parametrize(
-    "detector_z,detector_two_theta,phi_start,phi_end,phi_steps,exposure_time",
-    [(100, 30, 50, 60, 0.5, 0.2)],
-)
-@pytest.mark.parametrize(
-    "eh2_aperture",
+    "detector_z,detector_two_theta,phi_start,phi_end,phi_steps,exposure_time,eh2_aperture",
     [
-        (PinColRequest.PCOL20),
-        (PinColRequest.PCOL100),
+        (100.0, 30, 50.0, 60, 1, 0.2, PinColRequest.PCOL20),
+        (80.0, 90, 50.0, 60, 1, 0.2, PinColRequest.PCOL100),
     ],
 )
 @patch("i19_bluesky.serial.run_serial_with_panda.setup_beamline_before_collection")
+@patch("i19_bluesky.serial.run_serial_with_panda.trigger_panda")
 async def test_setup_then_trigger_panda(
+    mock_trigger_panda: MagicMock,
+    mock_setup_beamline_before_collection: MagicMock,
     detector_z: float,
     detector_two_theta: float,
     phi_start: float,
@@ -91,15 +128,7 @@ async def test_setup_then_trigger_panda(
     pincol: PinholeCollimatorControl,
     mock_panda: HDFPanda,
     RE: RunEngine,
-    mock_setup_beamline_before_collection: MagicMock,
-    mock_trigger_panda: MagicMock,
 ):
-    parent_mock = MagicMock()
-    parent_mock.attach_mock(
-        mock_setup_beamline_before_collection, "mock_setup_beamline_before_collection"
-    )
-    parent_mock.attach_mock(mock_trigger_panda, "mock_trigger_panda")
-
     RE(
         setup_then_trigger_panda(
             detector_z,
@@ -117,8 +146,6 @@ async def test_setup_then_trigger_panda(
     )
     mock_setup_beamline_before_collection.assert_called_once()
     mock_trigger_panda.assert_called_once()
-    assert eh2_diffractometer.det_stage.det_z == detector_z
-    assert eh2_diffractometer.det_stage.two_theta == detector_two_theta
 
 
 async def test_setup_diffractometer(
@@ -133,14 +160,12 @@ async def test_setup_diffractometer(
     mock_phi_velocity.assert_called_once_with(5.0)
 
 
-@patch("i19_bluesky.serial.example_trigger_plan_zebra_vs_panda.reset_panda")
-@patch("i19_bluesky.serial.example_trigger_plan_zebra_vs_panda.bps.sleep")
-@patch("i19_bluesky.serial.example_trigger_plan_zebra_vs_panda.disarm_panda")
-@patch("i19_bluesky.serial.example_trigger_plan_zebra_vs_panda.arm_panda")
-@patch(
-    "i19_bluesky.serial.example_trigger_plan_zebra_vs_panda.setup_panda_for_rotation"
-)
-@patch("i19_bluesky.serial.example_trigger_plan_zebra_vs_panda.setup_diffractometer")
+@patch("i19_bluesky.serial.run_serial_with_panda.reset_panda")
+@patch("i19_bluesky.serial.run_serial_with_panda.bps.sleep")
+@patch("i19_bluesky.serial.run_serial_with_panda.disarm_panda")
+@patch("i19_bluesky.serial.run_serial_with_panda.arm_panda")
+@patch("i19_bluesky.serial.run_serial_with_panda.setup_panda_for_rotation")
+@patch("i19_bluesky.serial.run_serial_with_panda.setup_diffractometer")
 async def test_trigger_panda(
     mock_setup_diffractometer: MagicMock,
     mock_setup_panda_for_rotation: MagicMock,
@@ -193,7 +218,7 @@ async def test_trigger_panda(
     parent_mock.assert_has_calls(expected_calls)
 
 
-@patch("i19_bluesky.serial.example_trigger_plan_zebra_vs_panda.disarm_panda")
+@patch("i19_bluesky.serial.run_serial_with_panda.disarm_panda")
 async def test_abort_panda(
     mock_disarm_panda: MagicMock,
     mock_panda: HDFPanda,
