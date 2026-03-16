a5dcb675bde45ce38c4539f9991bd9166240aab0
Matthew Carre <matthew.carre@diamond.ac.uk>
2026-03-16 15:22:06 +0000

    change in scope of files, moves tests to correct places

--- src/i19_bluesky/serial/example_trigger_plan_zebra_vs_panda.py
+++ src/i19_bluesky/serial/example_trigger_plan_zebra_vs_panda.py
@@ -8,12 +8,13 @@ from bluesky.utils import MsgGenerator
 from dodal.common import inject
 from dodal.devices.beamlines.i19.diffractometer import FourCircleDiffractometer
 from dodal.devices.zebra.zebra import RotationDirection, Zebra
-from ophyd_async.fastcs.panda import HDFPanda
 
 from i19_bluesky.eh2.zebra_arming_plan import arm_zebra, disarm_zebra
 from i19_bluesky.log import LOGGER
-from i19_bluesky.serial.panda_setup_plans import reset_panda, setup_panda_for_rotation
-from i19_bluesky.serial.panda_stubs import arm_panda, disarm_panda
+from i19_bluesky.serial.run_serial_with_panda import (
+    move_diffractometer_back,
+    setup_diffractometer,
+)
 from i19_bluesky.serial.zebra_collection_setup_plan import setup_zebra_for_collection
 
 RAMP = 0.5
@@ -106,105 +107,6 @@ def run_zebra_test(
             pulse_width,
         ),
         except_plan=lambda: (yield from abort_zebra(diffractometer, zebra)),
-        final_plan=lambda: (
-            yield from move_diffractometer_back(diffractometer, phi_start)
-        ),
-        auto_raise=False,
-    )
-
-
-def setup_diffractometer(
-    diffractometer: FourCircleDiffractometer,
-    phi_start: float,
-    phi_steps: int,
-    exposure_time: float,
-) -> MsgGenerator:
-    """Setup phi start posistion and velocity on the diffractometer.
-
-    Args:
-        diffractometer (FourCircleDiffractometer): The diffractometer ophyd device.
-        phi_start (float): Starting phi position.
-        phi_steps (int): Number of images to take.
-        exposure_time(float): Time between images, in seconds."""
-    yield from bps.abs_set(diffractometer.phi, phi_start)
-    velocity = phi_steps / exposure_time
-    yield from bps.abs_set(diffractometer.phi.velocity, velocity)
-
-
-def trigger_panda(
-    panda: HDFPanda,
-    diffractometer: FourCircleDiffractometer,
-    phi_start: float,
-    phi_end: float,
-    phi_steps: int,
-    exposure_time: float,
-) -> MsgGenerator:
-    """Trigger panda for collection in both directions.
-
-    Args:
-        panda (HDFPanda): The fastcs PandA ophyd device.
-        diffractometer (FourCircleDiffractometer): The diffractometer ophyd device.
-        phi_start (float): Starting phi position, in degrees.
-        phi_end (float): Ending phi position, in degrees.
-        phi_steps (int): Number of images to take.
-        exposure_time (float): Time between images, in seconds.
-    """
-    yield from setup_diffractometer(
-        diffractometer,
-        phi_start,
-        phi_steps,
-        exposure_time,
-    )
-    yield from setup_panda_for_rotation(
-        panda,
-        phi_start,
-        phi_end,
-        phi_steps,
-        exposure_time,
-    )
-    LOGGER.info("Arm panda and move phi")
-    yield from arm_panda(panda)
-    yield from bps.abs_set(diffractometer.phi, phi_end, wait=True)
-    yield from bps.sleep(2.0)
-    yield from bps.abs_set(diffractometer.phi, phi_start, wait=True)
-    LOGGER.info("Disarm panda")
-    yield from disarm_panda(panda)
-    yield from reset_panda(panda)
-
-
-def abort_panda(
-    diffractometer: FourCircleDiffractometer, panda: HDFPanda
-) -> MsgGenerator:
-    LOGGER.warning("ABORT")
-    yield from bps.abs_set(diffractometer.phi.motor_stop, 1, wait=True)
-    yield from disarm_panda(panda)
-
-
-def move_diffractometer_back(
-    diffractometer: FourCircleDiffractometer, phi_start: float
-) -> MsgGenerator:
-    LOGGER.info("Move diffractometer back to start position")
-    yield from bps.abs_set(diffractometer.phi, phi_start, wait=True)
-
-
-def run_panda_test(
-    phi_start: float,
-    phi_end: float,
-    phi_steps: int,
-    exposure_time: float,
-    diffractometer: FourCircleDiffractometer = inject("diffractometer"),
-    panda: HDFPanda = inject("panda"),
-) -> MsgGenerator:
-    yield from bpp.contingency_wrapper(
-        trigger_panda(
-            panda,
-            diffractometer,
-            phi_start,
-            phi_end,
-            phi_steps,
-            exposure_time,
-        ),
-        except_plan=lambda: (yield from abort_panda(diffractometer, panda)),
         final_plan=lambda: (
             yield from move_diffractometer_back(diffractometer, phi_start)
         ),
