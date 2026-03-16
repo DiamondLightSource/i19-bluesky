3fade352fc704196ecfb1a5f149381a1481e578c
Matthew Carre <matthew.carre@diamond.ac.uk>
2026-03-16 16:57:02 +0000

    Moves functions around for readability, fixed with precommit

--- tests/unit_tests/serial/test_run_serial_with_panda.py
+++ tests/unit_tests/serial/test_run_serial_with_panda.py
@@ -1,4 +1,4 @@
-from unittest.mock import MagicMock, call, patch
+from unittest.mock import MagicMock, patch
 
 import pytest
 from bluesky.run_engine import RunEngine
@@ -10,16 +10,11 @@ from dodal.devices.beamlines.i19.pin_col_stages import (
     PinColRequest,
     PinholeCollimatorControl,
 )
-from ophyd_async.core import get_mock_put
 from ophyd_async.fastcs.panda import HDFPanda
 
 from i19_bluesky.serial.run_serial_with_panda import (
-    abort_panda,
     main_entry_plan,
-    move_diffractometer_back,
-    setup_diffractometer,
     setup_then_trigger_panda,
-    trigger_panda,
 )
 
 
@@ -146,94 +141,3 @@ async def test_setup_then_trigger_panda(
     )
     mock_setup_beamline_before_collection.assert_called_once()
     mock_trigger_panda.assert_called_once()
-
-
-async def test_setup_diffractometer(
-    eh2_diffractometer: FourCircleDiffractometer,
-    RE: RunEngine,
-):
-    RE(setup_diffractometer(eh2_diffractometer, 6.0, 10, 2))
-    mock_phi = get_mock_put(eh2_diffractometer.phi.user_setpoint)
-    mock_phi.assert_called_once_with(6.0)
-
-    mock_phi_velocity = get_mock_put(eh2_diffractometer.phi.velocity)
-    mock_phi_velocity.assert_called_once_with(5.0)
-
-
-@patch("i19_bluesky.serial.run_serial_with_panda.reset_panda")
-@patch("i19_bluesky.serial.run_serial_with_panda.bps.sleep")
-@patch("i19_bluesky.serial.run_serial_with_panda.disarm_panda")
-@patch("i19_bluesky.serial.run_serial_with_panda.arm_panda")
-@patch("i19_bluesky.serial.run_serial_with_panda.setup_panda_for_rotation")
-@patch("i19_bluesky.serial.run_serial_with_panda.setup_diffractometer")
-async def test_trigger_panda(
-    mock_setup_diffractometer: MagicMock,
-    mock_setup_panda_for_rotation: MagicMock,
-    mock_arm_panda: MagicMock,
-    mock_disarm_panda: MagicMock,
-    mock_sleep: MagicMock,
-    mock_reset_panda: MagicMock,
-    mock_panda: HDFPanda,
-    eh2_diffractometer: FourCircleDiffractometer,
-    RE: RunEngine,
-):
-    parent_mock = MagicMock()
-    parent_mock.attach_mock(mock_setup_diffractometer, "mock_setup_diffractometer")
-    parent_mock.attach_mock(
-        mock_setup_panda_for_rotation, "mock_setup_panda_for_rotation"
-    )
-    parent_mock.attach_mock(mock_arm_panda, "mock_arm_panda")
-    parent_mock.attach_mock(mock_disarm_panda, "mock_disarm_panda")
-    parent_mock.attach_mock(mock_sleep, "mock_sleep")
-    parent_mock.attach_mock(mock_reset_panda, "mock_reset_panda")
-
-    RE(
-        trigger_panda(
-            panda=mock_panda,
-            diffractometer=eh2_diffractometer,
-            phi_start=5,
-            phi_end=10,
-            phi_steps=25,
-            exposure_time=10,
-        )
-    )
-    mock_phi = get_mock_put(eh2_diffractometer.phi.user_setpoint)
-    mock_phi.assert_has_calls([call(10), call(5)])
-
-    expected_calls = [
-        call.mock_setup_diffractometer(eh2_diffractometer, 5, 25, 10),
-        call.mock_setup_diffractometer().__iter__(),
-        call.mock_setup_panda_for_rotation(mock_panda, 5, 10, 25, 10),
-        call.mock_setup_panda_for_rotation().__iter__(),
-        call.mock_arm_panda(mock_panda),
-        call.mock_arm_panda().__iter__(),
-        call.mock_sleep(2.0),
-        call.mock_sleep().__iter__(),
-        call.mock_disarm_panda(mock_panda),
-        call.mock_disarm_panda().__iter__(),
-        call.mock_reset_panda(mock_panda),
-        call.mock_reset_panda().__iter__(),
-    ]
-
-    parent_mock.assert_has_calls(expected_calls)
-
-
-@patch("i19_bluesky.serial.run_serial_with_panda.disarm_panda")
-async def test_abort_panda(
-    mock_disarm_panda: MagicMock,
-    mock_panda: HDFPanda,
-    eh2_diffractometer: FourCircleDiffractometer,
-    RE: RunEngine,
-):
-    RE(abort_panda(eh2_diffractometer, mock_panda))
-    get_mock_put(eh2_diffractometer.phi.motor_stop).assert_called_once_with(1)
-    mock_disarm_panda.assert_called_once_with(mock_panda)
-
-
-async def test_move_diffractometer_back(
-    eh2_diffractometer: FourCircleDiffractometer,
-    RE: RunEngine,
-):
-    RE(move_diffractometer_back(eh2_diffractometer, 4.0))
-    mock_phi = get_mock_put(eh2_diffractometer.phi.user_setpoint)
-    mock_phi.assert_called_once_with(4.0)
