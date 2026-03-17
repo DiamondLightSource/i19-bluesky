ab468bd8b9e5dc7bc489b9b258682c6cc3777100
Matthew Carre <matthew.carre@diamond.ac.uk>
2026-03-17 15:22:44 +0000

    fixes ordering, fixes tests

--- tests/unit_tests/serial/test_setup_beamline_before_collection.py
+++ tests/unit_tests/serial/test_setup_beamline_before_collection.py
@@ -1,7 +1,6 @@
-from unittest.mock import MagicMock
-
 import pytest
 from bluesky.run_engine import RunEngine
+from dodal.common.enums import InOutUpper
 from dodal.devices.beamlines.i19.backlight import BacklightPosition
 from dodal.devices.beamlines.i19.diffractometer import (
     FourCircleDiffractometer,
@@ -10,6 +9,7 @@ from dodal.devices.beamlines.i19.pin_col_stages import (
     PinColRequest,
     PinholeCollimatorControl,
 )
+from ophyd_async.core import set_mock_value
 
 from i19_bluesky.serial.setup_beamline_pre_collection import (
     setup_beamline_before_collection,
@@ -17,32 +17,36 @@ from i19_bluesky.serial.setup_beamline_pre_collection import (
 
 
 @pytest.mark.parametrize(
-    "detector_z,detector_two_theta", [(200, 0), (100, 30), (80, 90)]
-)
-@pytest.mark.parametrize(
-    "eh2_aperture",
+    "detector_z,detector_two_theta,eh2_aperture,in_positions",
     [
-        (PinColRequest.PCOL20),
-        (PinColRequest.PCOL100),
+        (200, 0, PinColRequest.PCOL20, [12, 34.5, 7.1, 28]),
+        (80, 90, PinColRequest.PCOL100, [23.4, 22.1, 12, 18.7]),
     ],
 )
 async def test_setup_beamline_before_collection(
     detector_z: float,
     detector_two_theta: float,
-    eh2_diffractometer: FourCircleDiffractometer,
+    eh2_aperture: PinColRequest,
+    in_positions: list[float],
     RE: RunEngine,
+    eh2_diffractometer: FourCircleDiffractometer,
     eh2_backlight: BacklightPosition,
     pincol: PinholeCollimatorControl,
-    eh2_aperture: PinColRequest,
 ):
+
+    size = int(eh2_aperture.value.strip("um"))
+    set_mock_value(pincol.mapt.pin_x.in_positions[size], in_positions[0])
+    set_mock_value(pincol.mapt.pin_y.in_positions[size], in_positions[1])
+    set_mock_value(pincol.mapt.col_x.in_positions[size], in_positions[2])
+    set_mock_value(pincol.mapt.col_y.in_positions[size], in_positions[3])
     RE(
         setup_beamline_before_collection(
             detector_z,
             detector_two_theta,
+            eh2_aperture,
             eh2_backlight,
             eh2_diffractometer,
             pincol,
-            eh2_aperture,
         )
     )
     assert (
@@ -52,15 +56,10 @@ async def test_setup_beamline_before_collection(
         await eh2_diffractometer.det_stage.two_theta.user_readback.get_value()
         == detector_two_theta
     )
-    mock_setup_beamline_before_collection = MagicMock()
-    RE(
-        mock_setup_beamline_before_collection(
-            detector_z,
-            detector_two_theta,
-            eh2_backlight,
-            eh2_diffractometer,
-            pincol,
-            eh2_aperture,
-        )
-    )
-    mock_setup_beamline_before_collection.assert_called_once()
+
+    assert await eh2_backlight.position.get_value() == InOutUpper.OUT
+
+    assert await pincol._pinhole.x.user_readback.get_value() == in_positions[0]
+    assert await pincol._pinhole.y.user_readback.get_value() == in_positions[1]
+    assert await pincol._collimator.x.user_readback.get_value() == in_positions[2]
+    assert await pincol._collimator.y.user_readback.get_value() == in_positions[3]
