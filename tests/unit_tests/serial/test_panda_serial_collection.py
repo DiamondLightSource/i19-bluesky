c002abc3d8fb5ec09334e934eced124b9918aad7
Matt-Carre <matthew.carre@diamond.ac.uk>
2026-03-25 15:02:33 +0000

    Adding arm/disarm Eiger detector to serial entry point (#142)
    * Added tests for moving detector stage
    
    * Added tests for moving detector stage, corrected logic.
    
    * Finalised move detector stage plan + tests
    
    * Added plans that form basis to setting up detector
    
    * Added logging to detector setup
    
    * Renamed and simplified added new files
    
    * edited conftest.py
    
    * Reordered beamline pre collection setup arguments
    
    * Altered tests
    
    * Update uv.lock
    
    * Finalised move detector stage plan + tests
    
    * fixed uv.lock again?
    
    * Found the other conflict and sorted it?
    
    * deleted obselete file
    
    * Removed extra files, renamed move_stage
    
    * Modified uv.lock to pass the final linting check
    
    * Initial commit, added main entry point code. Tests being written now.
    
    * Added main entry point code properly
    
    * adding quick fixes
    
    * Altered tests, UV.lock
    
    * changed test of setup_beamline_before_collection
    
    * change in scope of files, moves tests to correct places
    
    * rename zebra mock path
    
    * rename zebra mock paths missed by previous commit
    
    * rename zebra mock paths missed by previous commit, fixes __init__ for same reason
    
    * rename zebra mock paths missed by previous commit, fixes __init__ for same reason, again but this time without mistakes
    
    * Moves functions around for readability, fixed with precommit
    
    * halfway through adding eiger functionality
    
    * attempting to add fixture for eiger
    
    * Adds eiger controlling to trigger_panda. Needs verifying if this is the right approach
    
    * uploading working version
    
    * adds tests, ready for review
    
    * Edited merging mistakes
    
    * altered eiger class
    
    * edited other file
    
    * edited other files
    
    ---------
    
    Co-authored-by: Noemi Frisina <noemi.frisina@diamond.ac.uk>

--- tests/unit_tests/serial/test_panda_serial_collection.py
+++ tests/unit_tests/serial/test_panda_serial_collection.py
@@ -5,6 +5,7 @@ from dodal.devices.beamlines.i19.diffractometer import (
     FourCircleDiffractometer,
 )
 from ophyd_async.core import get_mock_put
+from ophyd_async.fastcs.eiger import EigerDetector
 from ophyd_async.fastcs.panda import HDFPanda
 
 from i19_bluesky.serial.panda_serial_collection import (
@@ -27,6 +28,7 @@ async def test_setup_diffractometer(
     mock_phi_velocity.assert_called_once_with(5.0)
 
 
+@patch("i19_bluesky.serial.panda_serial_collection.bps.trigger")
 @patch("i19_bluesky.serial.panda_serial_collection.reset_panda")
 @patch("i19_bluesky.serial.panda_serial_collection.bps.sleep")
 @patch("i19_bluesky.serial.panda_serial_collection.disarm_panda")
@@ -40,7 +42,9 @@ async def test_trigger_panda(
     mock_disarm_panda: MagicMock,
     mock_sleep: MagicMock,
     mock_reset_panda: MagicMock,
+    mock_arm_or_disarm: MagicMock,
     mock_panda: HDFPanda,
+    eh2_eiger: EigerDetector,
     eh2_diffractometer: FourCircleDiffractometer,
     RE: RunEngine,
 ):
@@ -53,15 +57,16 @@ async def test_trigger_panda(
     parent_mock.attach_mock(mock_disarm_panda, "mock_disarm_panda")
     parent_mock.attach_mock(mock_sleep, "mock_sleep")
     parent_mock.attach_mock(mock_reset_panda, "mock_reset_panda")
-
+    parent_mock.attach_mock(mock_arm_or_disarm, "mock_arm_or_disarm")
     RE(
         trigger_panda(
-            panda=mock_panda,
-            diffractometer=eh2_diffractometer,
             phi_start=5,
             phi_end=10,
             phi_steps=25,
             exposure_time=10,
+            panda=mock_panda,
+            diffractometer=eh2_diffractometer,
+            eiger=eh2_eiger,
         )
     )
     mock_phi = get_mock_put(eh2_diffractometer.phi.user_setpoint)
@@ -71,8 +76,10 @@ async def test_trigger_panda(
         call.mock_setup_diffractometer(eh2_diffractometer, 5, 25, 10),
         call.mock_setup_panda_for_rotation(mock_panda, 5, 10, 25, 10),
         call.mock_arm_panda(mock_panda),
+        call.mock_arm_or_disarm(eh2_eiger.drv.detector.arm),
         call.mock_sleep(2.0),
         call.mock_disarm_panda(mock_panda),
+        call.mock_arm_or_disarm(eh2_eiger.drv.detector.disarm),
         call.mock_reset_panda(mock_panda),
     ]
 
