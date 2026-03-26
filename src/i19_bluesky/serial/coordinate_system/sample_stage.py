525e51b19eb7f557c9cfd6d0dd51dfc80e34a11a
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2026-03-26 16:03:34 +0000

    Add docstrings

--- src/i19_bluesky/serial/coordinate_system/sample_stage.py
+++ src/i19_bluesky/serial/coordinate_system/sample_stage.py
@@ -7,6 +7,16 @@ from dodal.devices.beamlines.i19.diffractometer import FourCircleDiffractometer
 def read_current_sample_stage_xyz_position(
     diffractometer: FourCircleDiffractometer = inject("diffractometer"),
 ) -> MsgGenerator[tuple[float, float, float]]:
+    """Read the current position of the x,y,z motors of the sample stage on the
+    diffractometer and return the values as a tuple.
+
+    Args:
+        diffractometer (FourCircleDiffractometer, optional): The diffractometer device.
+
+    Returns:
+        MsgGenerator[tuple[float, float, float]]: The (x,y,z) positions of the sample
+        stage.
+    """
     x = yield from bps.rd(diffractometer.x)
     y = yield from bps.rd(diffractometer.y)
     z = yield from bps.rd(diffractometer.z)
@@ -17,6 +27,12 @@ def move_sample_stage_to_corners(
     corner_coord: tuple[float, float, float],
     diffractometer: FourCircleDiffractometer = inject("diffractometer"),
 ) -> MsgGenerator:
+    """Move the x,y,z motors of the sample stage to the fiducial positions.
+
+    Args:
+        corner_coord (tuple[float, float, float]): Coordinates of the fiducial corner.
+        diffractometer (FourCircleDiffractometer, optional): The diffractometer device.
+    """
     yield from bps.mv(
         diffractometer.x,
         corner_coord[0],
