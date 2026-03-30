e68ff3404141b07427ba1291d45773b7da61963e
Matthew Carre <matthew.carre@diamond.ac.uk>
2026-03-30 17:14:18 +0100

    Made UI plans discoverable

--- src/i19_bluesky/serial/__init__.py
+++ src/i19_bluesky/serial/__init__.py
@@ -6,5 +6,18 @@ from i19_bluesky.serial.example_zebra_plans.example_trigger_plan_zebra_vs_panda 
 from i19_bluesky.serial.run_panda_plans.run_serial_with_panda import (
     run_serial_with_panda,
 )
+from i19_bluesky.serial.ui_plans.ui_plans import (
+    move_backlight_in,
+    move_backlight_in_via_ui,
+    move_backlight_in_via_ui_quick,
+    rotate_in_phi,
+)
 
-__all__ = ["run_zebra_test", "run_serial_with_panda"]
+__all__ = [
+    "run_zebra_test",
+    "run_serial_with_panda",
+    "rotate_in_phi",
+    "move_backlight_in_via_ui_quick",
+    "move_backlight_in_via_ui",
+    "move_backlight_in",
+]
