eca25b1c7e60d8fb1fbba08094c8b4e2568cc0d2
Matthew Carre <matthew.carre@diamond.ac.uk>
2026-03-31 14:17:33 +0100

    made plans discoverable by bluesky

--- src/i19_bluesky/serial/__init__.py
+++ src/i19_bluesky/serial/__init__.py
@@ -1,5 +1,6 @@
 """All things serial go here."""
 
+from i19_bluesky.eh2.backlight_plan import move_backlight_out
 from i19_bluesky.serial.example_zebra_plans.example_trigger_plan_zebra_vs_panda import (
     run_zebra_test,
 )
@@ -7,7 +8,6 @@ from i19_bluesky.serial.run_panda_plans.run_serial_with_panda import (
     run_serial_with_panda,
 )
 from i19_bluesky.serial.ui_plans.ui_plans import (
-    move_backlight_in,
     move_backlight_in_via_ui,
     move_backlight_in_via_ui_quick,
     rotate_in_phi,
@@ -19,5 +19,5 @@ __all__ = [
     "rotate_in_phi",
     "move_backlight_in_via_ui_quick",
     "move_backlight_in_via_ui",
-    "move_backlight_in",
+    "move_backlight_out",
 ]
