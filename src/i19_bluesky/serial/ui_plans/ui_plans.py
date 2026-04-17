686f46ac8b50f6345d65b58c0a948a8960fd59bf
Matthew Carre <matthew.carre@diamond.ac.uk>
2026-04-17 11:03:33 +0100

    alters based on further comments

--- src/i19_bluesky/serial/ui_plans/ui_plans.py
+++ src/i19_bluesky/serial/ui_plans/ui_plans.py
@@ -3,6 +3,7 @@ from bluesky.utils import MsgGenerator
 from dodal.common import inject
 
 from i19_bluesky.eh2.backlight_plan import move_backlight_in
+from i19_bluesky.parameters.components import BacklightOption
 from i19_bluesky.parameters.devices_composites import SerialCollectionEh2PandaComposite
 from i19_bluesky.serial.device_setup_plans.diffractometer_plans import (
     move_detector_stage,
@@ -10,24 +11,22 @@ from i19_bluesky.serial.device_setup_plans.diffractometer_plans import (
 
 
 def move_backlight_in_via_ui(
-    param: dict,
+    option: BacklightOption,
     devices: SerialCollectionEh2PandaComposite = inject(""),
 ) -> MsgGenerator:
-    match param["option"]:
-        case "slow":
+    match option:
+        case option.SLOW:
             det_z, two_theta = 250.0, 90.0
-        case "quick":
+        case option.QUICK:
             det_z, two_theta = 100.0, 0.0
-        case _:
-            det_z, two_theta = 0.0, 0.0
     yield from move_detector_stage(devices.diffractometer.det_stage, det_z, two_theta)
     yield from move_backlight_in(devices.backlight)
 
 
 def rotate_in_phi(
-    params: dict,
+    rot_increment: float,
     devices: SerialCollectionEh2PandaComposite = inject(""),
 ) -> MsgGenerator:
     rot_axis_start = yield from bps.rd(devices.diffractometer.phi.user_readback)
-    rot_axis_end = params["rot_axis_increment"] + rot_axis_start
+    rot_axis_end = rot_increment + rot_axis_start
     yield from bps.abs_set(devices.diffractometer.phi, rot_axis_end, wait=True)
--- tests/unit_tests/serial/ui_plans/test_ui_plans.py
+++ tests/unit_tests/serial/ui_plans/test_ui_plans.py
@@ -4,7 +4,9 @@ import pytest
 from bluesky.run_engine import RunEngine
 from ophyd_async.core import set_mock_value
 
+from i19_bluesky.parameters.components import BacklightOption
 from i19_bluesky.parameters.devices_composites import SerialCollectionEh2PandaComposite
+from i19_bluesky.parameters.serial_parameters import SerialExperimentEh2
 from i19_bluesky.serial.ui_plans.ui_plans import (
     move_backlight_in_via_ui,
     rotate_in_phi,
@@ -12,31 +14,21 @@ from i19_bluesky.serial.ui_plans.ui_plans import (
 
 
 @pytest.mark.parametrize(
-    "param,det_z,two_theta",
-    [
-        ({"option": "slow"}, 250, 90),
-        (
-            {
-                "option": "quick",
-            },
-            100,
-            0,
-        ),
-        ({"option": "???"}, 0, 0),
-    ],
+    "option,det_z,two_theta",
+    [(BacklightOption.SLOW, 250, 90), (BacklightOption.QUICK, 100, 0)],
 )
 @patch("i19_bluesky.serial.ui_plans.ui_plans.move_detector_stage")
 @patch("i19_bluesky.serial.ui_plans.ui_plans.move_backlight_in")
 async def test_move_backlight_in_via_ui(
     mock_move_backlight_in: MagicMock,
     mock_move_detector_stage: MagicMock,
-    param: dict,
+    option: BacklightOption,
     det_z: float,
     two_theta: float,
     RE: RunEngine,
     devices: SerialCollectionEh2PandaComposite,
 ):
-    RE(move_backlight_in_via_ui(param, devices))
+    RE(move_backlight_in_via_ui(option, devices))
     mock_move_detector_stage.assert_called_once_with(
         devices.diffractometer.det_stage, det_z, two_theta
     )
@@ -44,32 +36,31 @@ async def test_move_backlight_in_via_ui(
 
 
 @pytest.mark.parametrize(
-    "params",
+    "rot_increment",
     [
-        ({"rot_axis_increment": 10.0}),
-        ({"rot_axis_increment": 50.0}),
-        ({"rot_axis_increment": -4.0}),
+        (10.0),
+        (50.0),
+        (-4.0),
     ],
 )
 @patch("i19_bluesky.serial.ui_plans.ui_plans.bps.abs_set")
 async def test_rotate_in_phi(
     mock_abs_set: MagicMock,
     RE: RunEngine,
-    params: dict,
+    rot_increment: float,
+    parameters: SerialExperimentEh2,
     devices: SerialCollectionEh2PandaComposite,
 ):
 
-    rot_axis_end = (
-        params["rot_axis_increment"] + 0
-    )  # 0 = start position of diffractometer
-    RE(rotate_in_phi(params, devices))
+    rot_axis_end = rot_increment + parameters.rot_axis_start
+    RE(rotate_in_phi(rot_increment, devices))
     mock_abs_set.assert_called_with(
         devices.diffractometer.phi,
         rot_axis_end,
         wait=True,
     )
     set_mock_value(devices.diffractometer.phi.user_readback, rot_axis_end)
-    RE(rotate_in_phi(params, devices))
+    RE(rotate_in_phi(rot_increment, devices))
     mock_abs_set.assert_called_with(
         devices.diffractometer.phi, rot_axis_end + rot_axis_end, wait=True
     )
