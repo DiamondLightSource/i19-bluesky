ea31503adf154ab03e6d57d8b39658fae90d5a4b
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2026-04-20 13:28:45 +0100

    Use the new sample stages and fix some tests

--- src/i19_bluesky/serial/coordinate_system/sample_stage.py
+++ src/i19_bluesky/serial/coordinate_system/sample_stage.py
@@ -1,43 +1,43 @@
 import bluesky.plan_stubs as bps
 from bluesky.utils import MsgGenerator
 from dodal.common import inject
-from dodal.devices.beamlines.i19.diffractometer import FourCircleDiffractometer
+from dodal.devices.motors import XYZPhiStage
 
 
 def read_current_sample_stage_xyz_position(
-    diffractometer: FourCircleDiffractometer = inject("diffractometer"),
+    sample_stage: XYZPhiStage = inject("serial_stages"),
 ) -> MsgGenerator[tuple[float, float, float]]:
     """Read the current position of the x,y,z motors of the sample stage on the
     diffractometer and return the values as a tuple.
 
     Args:
-        diffractometer (FourCircleDiffractometer, optional): The diffractometer device.
+        sample_stage (XYZPhiStage, optional): The serial sample stage device.
 
     Returns:
         MsgGenerator[tuple[float, float, float]]: The (x,y,z) positions of the sample
         stage.
     """
-    x = yield from bps.rd(diffractometer.x)
-    y = yield from bps.rd(diffractometer.y)
-    z = yield from bps.rd(diffractometer.z)
+    x = yield from bps.rd(sample_stage.x)
+    y = yield from bps.rd(sample_stage.y)
+    z = yield from bps.rd(sample_stage.z)
     return (x, y, z)
 
 
 def move_sample_stage_to_corners(
     corner_coord: tuple[float, float, float],
-    diffractometer: FourCircleDiffractometer = inject("diffractometer"),
+    sample_stage: XYZPhiStage = inject("serial_stages"),
 ) -> MsgGenerator:
     """Move the x,y,z motors of the sample stage to the fiducial positions.
 
     Args:
         corner_coord (tuple[float, float, float]): Coordinates of the fiducial corner.
-        diffractometer (FourCircleDiffractometer, optional): The diffractometer device.
+        sample_stage (XYZPhiStage, optional): The serial sample stage device.
     """
     yield from bps.mv(
-        diffractometer.x,
+        sample_stage.x,
         corner_coord[0],
-        diffractometer.y,
+        sample_stage.y,
         corner_coord[1],
-        diffractometer.z,
+        sample_stage.z,
         corner_coord[2],
     )
--- tests/unit_tests/serial/coordinate_system/test_sample_stage.py
+++ tests/unit_tests/serial/coordinate_system/test_sample_stage.py
@@ -1,5 +1,5 @@
 from bluesky import RunEngine
-from dodal.devices.beamlines.i19.diffractometer import FourCircleDiffractometer
+from dodal.devices.motors import XYZPhiStage
 from ophyd_async.core import set_mock_value
 
 from i19_bluesky.serial.coordinate_system.sample_stage import (
@@ -8,23 +8,19 @@ from i19_bluesky.serial.coordinate_system.sample_stage import (
 )
 
 
-def test_read_current_xyz_position(
-    eh2_diffractometer: FourCircleDiffractometer, RE: RunEngine
-):
-    set_mock_value(eh2_diffractometer.x.user_readback, 2.4)
-    set_mock_value(eh2_diffractometer.y.user_readback, 0.0)
-    set_mock_value(eh2_diffractometer.z.user_readback, 0.5)
+def test_read_current_xyz_position(serial_stages: XYZPhiStage, RE: RunEngine):
+    set_mock_value(serial_stages.x.user_readback, 2.4)
+    set_mock_value(serial_stages.y.user_readback, 0.0)
+    set_mock_value(serial_stages.z.user_readback, 0.5)
 
-    res = RE(read_current_sample_stage_xyz_position(eh2_diffractometer)).plan_result  # type: ignore
+    res = RE(read_current_sample_stage_xyz_position(serial_stages)).plan_result  # type: ignore
 
     assert res == (2.4, 0.0, 0.5)
 
 
-async def test_move_sample_to_position(
-    eh2_diffractometer: FourCircleDiffractometer, RE: RunEngine
-):
-    RE(move_sample_stage_to_corners((0.1, 1.2, 0.8), eh2_diffractometer))
+async def test_move_sample_to_position(serial_stages: XYZPhiStage, RE: RunEngine):
+    RE(move_sample_stage_to_corners((0.1, 1.2, 0.8), serial_stages))
 
-    assert await eh2_diffractometer.x.user_readback.get_value() == 0.1
-    assert await eh2_diffractometer.y.user_readback.get_value() == 1.2
-    assert await eh2_diffractometer.z.user_readback.get_value() == 0.8
+    assert await serial_stages.x.user_readback.get_value() == 0.1
+    assert await serial_stages.y.user_readback.get_value() == 1.2
+    assert await serial_stages.z.user_readback.get_value() == 0.8
