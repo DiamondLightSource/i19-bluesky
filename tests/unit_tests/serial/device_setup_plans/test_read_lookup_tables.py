f1be66f378e32f76167b137b1137e7c6aee70a03
Matthew Carre <matthew.carre@diamond.ac.uk>
2026-05-20 13:15:56 +0100

    changes test to use mocks not the real thing

--- tests/unit_tests/serial/device_setup_plans/test_read_lookup_tables.py
+++ tests/unit_tests/serial/device_setup_plans/test_read_lookup_tables.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 from daq_config_server.models.lookup_tables import DetectorXYLookupTable
 
 from i19_bluesky.serial.device_setup_plans.read_lookup_tables import (
@@ -5,9 +7,22 @@ from i19_bluesky.serial.device_setup_plans.read_lookup_tables import (
 )
 
 
+@patch(
+    "i19_bluesky.serial.device_setup_plans.read_lookup_tables.config_client",
+)
 # check if one value (chosen arbitrarily) does provide the correct corresponding value
-def test_read_eiger_lookup_table():
-    assert isinstance(read_eiger_lookup_table(), DetectorXYLookupTable)
+def test_read_eiger_lookup_table(mock_client):
+    content = """
+#Table giving centre position of beam X and Y as a function of detector distance
+#
+# Columns: detector distance, x-centre, y-centre
+Units mm mm mm
+85  74.25 106.67
+585 74.25 106.67
+"""
+    returned_file = DetectorXYLookupTable.from_contents(content)
+    mock_client.return_value.get_file_contents.return_value = returned_file
+
     assert (
         read_eiger_lookup_table().get_value(
             search_column_name="beam_centre_x_mm",
