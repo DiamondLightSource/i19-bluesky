1fee49f8e0c86f70a8c3c884c235f7f07ce86fd4
Matthew Carre <matthew.carre@diamond.ac.uk>
2026-05-20 15:37:20 +0100

    now returns a tuple

--- src/i19_bluesky/serial/device_setup_plans/read_lookup_tables.py
+++ src/i19_bluesky/serial/device_setup_plans/read_lookup_tables.py
@@ -2,10 +2,14 @@ from daq_config_server.models.lookup_tables import DetectorXYLookupTable
 from dodal.beamlines.i19_2 import config_client
 
 
-def read_eiger_lookup_table():
+def _read_eiger_lookup_table():
     return config_client().get_file_contents(
         "/dls_sw/i19-2/software/daq_configuration/lookup/"
         "DetDistToBeamXYConverterE4M.txt",
         DetectorXYLookupTable,
         force_parser=DetectorXYLookupTable.from_contents,
     )
+
+
+def find_beam_xy(index: int):
+    return tuple(_read_eiger_lookup_table().columns[index])
--- tests/unit_tests/serial/device_setup_plans/test_read_lookup_tables.py
+++ tests/unit_tests/serial/device_setup_plans/test_read_lookup_tables.py
@@ -3,16 +3,11 @@ from unittest.mock import patch
 from daq_config_server.models.lookup_tables import DetectorXYLookupTable
 
 from i19_bluesky.serial.device_setup_plans.read_lookup_tables import (
-    read_eiger_lookup_table,
+    _read_eiger_lookup_table,
+    find_beam_xy,
 )
 
-
-@patch(
-    "i19_bluesky.serial.device_setup_plans.read_lookup_tables.config_client",
-)
-# check if one value (chosen arbitrarily) does provide the correct corresponding value
-def test_read_eiger_lookup_table(mock_client):
-    content = """
+CONTENT = """
 #Table giving centre position of beam X and Y as a function of detector distance
 #
 # Columns: detector distance, x-centre, y-centre
@@ -20,14 +15,28 @@ Units mm mm mm
 85  74.25 106.67
 585 74.25 106.67
 """
-    returned_file = DetectorXYLookupTable.from_contents(content)
+
+
+@patch(
+    "i19_bluesky.serial.device_setup_plans.read_lookup_tables.config_client",
+)
+# check if one value (chosen arbitrarily) does provide the correct corresponding value
+def test_read_eiger_lookup_table(mock_client):
+    returned_file = DetectorXYLookupTable.from_contents(CONTENT)
     mock_client.return_value.get_file_contents.return_value = returned_file
 
     assert (
-        read_eiger_lookup_table().get_value(
+        _read_eiger_lookup_table().get_value(
             search_column_name="beam_centre_x_mm",
             value=74.25,
             target_column_name="beam_centre_y_mm",
         )
         == 106.67
     )
+
+
+@patch("i19_bluesky.serial.device_setup_plans.read_lookup_tables.config_client")
+def test_find_beam_xy(mock_client):
+    returned_file = DetectorXYLookupTable.from_contents(CONTENT)
+    mock_client.return_value.get_file_contents.return_value = returned_file
+    assert find_beam_xy(0) == (85.0, 585.0)
