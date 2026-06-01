ff192d00c30ff263849dafa5ed2b11217a78b460
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2026-06-01 17:10:15 +0100

    Tidy up a bit

--- src/i19_bluesky/optics/change_energy_plans.py
+++ src/i19_bluesky/optics/change_energy_plans.py
@@ -6,7 +6,7 @@ from dodal.common import inject
 from dodal.common.beamlines.beamline_utils import (
     get_config_client,
 )
-from dodal.devices.beamlines.i19.mirror_stripes import MirrorStripes, StripeChoice
+from dodal.devices.beamlines.i19.mirror_stripes import StripeChoice
 from dodal.devices.common_dcm import DoubleCrystalMonochromatorWithDSpacing
 from dodal.devices.focusing_mirror import FocusingMirrorWithPiezo
 from dodal.devices.undulator import UndulatorInKeV
@@ -26,9 +26,12 @@ def change_energy_plan(
     stripe_choice: StripeChoice,
     devices: SetEnergyComposite = inject(),
 ) -> MsgGenerator:
-    LOGGER.info(f"Changing the energy to {energy_in_kev} KeV")
+    LOGGER.info(f"Change the energy to {energy_in_kev} KeV")
     yield from _set_energy(energy_in_kev, devices.dcm, devices.undulator)
-    yield from _drive_mirror_stripes(stripe_choice, devices.mirror_stripes)
+    LOGGER.info(f"Drive mirrors to {stripe_choice.value} stripe and set voltages")
+    yield from bps.abs_set(
+        devices.mirror_stripes.stripe_choice, stripe_choice, wait=True
+    )
     yield from _apply_piezo_voltages(stripe_choice, devices.hfm, devices.vfm)
 
 
@@ -39,16 +42,11 @@ def _set_energy(
     group: str = "drive-dcm-and-id-gap",
     wait: bool = True,
 ):
-    # Move dcm energy and undulator ID gap to new energy
+    LOGGER.info("Drive the dcm energy and ID gap")
     yield from bps.abs_set(undulator, energy_in_kev, group=group)
     yield from bps.abs_set(dcm.energy_in_keV, energy_in_kev, group=group)
     if wait:
         yield from bps.wait(group=group)
-
-
-def _drive_mirror_stripes(stripe_choice: StripeChoice, mirror_stripes: MirrorStripes):
-    # Set stripe choice depending on EH and energy
-    yield from bps.abs_set(mirror_stripes.stripe_choice, stripe_choice, wait=True)
 
 
 def _get_piezo_voltages_to_set_from_config_client() -> dict:
@@ -64,8 +62,12 @@ def _apply_piezo_voltages(
     hfm: FocusingMirrorWithPiezo,
     vfm: FocusingMirrorWithPiezo,
     group: str = "apply-voltage-to-piezo-actuators",
+    wait: bool = True,
 ):
     piezo_voltages_config = _get_piezo_voltages_to_set_from_config_client()
     voltages_to_set = piezo_voltages_config[stripe_choice.value]
+    LOGGER.info(f"Set piezo voltages to: {voltages_to_set}")
     yield from bps.abs_set(hfm.piezo, voltages_to_set["hfm"], group=group)
     yield from bps.abs_set(vfm.piezo, voltages_to_set["vfm"], group=group)
+    if wait:
+        yield from bps.wait(group=group)
