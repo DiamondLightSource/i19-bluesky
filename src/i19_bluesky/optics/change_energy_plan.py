f65e1173f4e279332a8e486c7646eeca359ff494
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2026-06-01 17:01:34 +0100

    Get the piezo voltages out of a json file using the config client

--- src/i19_bluesky/optics/change_energy_plan.py
+++ src/i19_bluesky/optics/change_energy_plan.py
@@ -1,6 +1,11 @@
+from pathlib import Path
+
 import bluesky.plan_stubs as bps
 from bluesky.utils import MsgGenerator
 from dodal.common import inject
+from dodal.common.beamlines.beamline_utils import (
+    get_config_client,
+)
 from dodal.devices.beamlines.i19.mirror_stripes import MirrorStripes, StripeChoice
 from dodal.devices.common_dcm import DoubleCrystalMonochromatorWithDSpacing
 from dodal.devices.focusing_mirror import FocusingMirrorWithPiezo
@@ -10,12 +15,9 @@ from i19_bluesky.log import LOGGER
 from i19_bluesky.optics.check_access_control import check_access
 from i19_bluesky.optics.device_composites import SetEnergyComposite
 
-# TODO On request from scientist, this needs to be a JSON file readable with config
-# server as not known how often they'll need to be checked
-PIEZO_VOLTAGES = {
-    StripeChoice.EH1_RH: {"hfm": 1.600, "vfm": 1.720},
-    StripeChoice.EH2_PT: {"hfm": 3.545, "vfm": 2.275},
-}  # For a start
+PIEZO_VOLTAGES_JSON_PATH = Path(
+    "/dls_sw/i19-1/software/i19-acquisition/i19-shared/json/PiezoVoltages.json"
+)
 
 
 @check_access
@@ -49,12 +51,21 @@ def _drive_mirror_stripes(stripe_choice: StripeChoice, mirror_stripes: MirrorStr
     yield from bps.abs_set(mirror_stripes.stripe_choice, stripe_choice, wait=True)
 
 
+def _get_piezo_voltages_to_set_from_config_client() -> dict:
+    config_client = get_config_client()
+    piezo_voltages = config_client.get_file_contents(
+        PIEZO_VOLTAGES_JSON_PATH, desired_return_type=dict
+    )
+    return piezo_voltages
+
+
 def _apply_piezo_voltages(
     stripe_choice: StripeChoice,
     hfm: FocusingMirrorWithPiezo,
     vfm: FocusingMirrorWithPiezo,
     group: str = "apply-voltage-to-piezo-actuators",
 ):
-    voltages_to_set = PIEZO_VOLTAGES[stripe_choice]
+    piezo_voltages_config = _get_piezo_voltages_to_set_from_config_client()
+    voltages_to_set = piezo_voltages_config[stripe_choice.value]
     yield from bps.abs_set(hfm.piezo, voltages_to_set["hfm"], group=group)
     yield from bps.abs_set(vfm.piezo, voltages_to_set["vfm"], group=group)
