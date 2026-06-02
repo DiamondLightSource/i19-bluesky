57f41268434fcd784701d8d32c762847bef1af94
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2026-06-02 10:41:10 +0100

    Add dependency on config server

---
+++ tests/unit_tests/optics/test_change_energy_plans.py
@@ -0,0 +1,14 @@
+async def test_set_energy():
+    pass
+
+
+async def test_apply_piezo_voltages():
+    pass
+
+
+async def test_energy_not_changed_if_requested_from_wrong_hutch():
+    pass
+
+
+async def test_change_energy_plan():
+    pass
