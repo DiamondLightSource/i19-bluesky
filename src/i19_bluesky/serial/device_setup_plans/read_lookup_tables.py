18996168490598760a1de88403054697b813ac67
Matthew Carre <matthew.carre@diamond.ac.uk>
2026-06-19 09:39:17 +0100

    Fixes code based on feedback

--- src/i19_bluesky/serial/device_setup_plans/read_lookup_tables.py
+++ src/i19_bluesky/serial/device_setup_plans/read_lookup_tables.py
@@ -1,11 +1,17 @@
 from daq_config_server.models.lookup_tables import DetectorXYLookupTable
-from dodal.beamlines.i19_2 import config_client
+from dodal.common.beamlines.beamline_utils import (
+    get_config_client,
+)
+
+BEAM_XY_TABLE_PATH = (
+    "/dls_sw/i19-2/software/daq_configuration/lookup/DetDistToBeamXYConverterE4M.txt"
+)
 
 
 def _read_eiger_lookup_table():
-    return config_client().get_file_contents(
-        "/dls_sw/i19-2/software/daq_configuration/lookup/"
-        "DetDistToBeamXYConverterE4M.txt",
+    config_client = get_config_client()
+    return config_client.get_file_contents(
+        BEAM_XY_TABLE_PATH,
         DetectorXYLookupTable,
         force_parser=DetectorXYLookupTable.from_contents,
     )
