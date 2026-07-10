2cb42059e2abba38b58890c094c504578fbd5c7b
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2026-07-10 17:54:49 +0100

    Add comment

--- src/i19_bluesky/eh1/find_beam_centre.py
+++ src/i19_bluesky/eh1/find_beam_centre.py
@@ -11,11 +11,10 @@ def find_beam_centre_plan(
     beam_centre: CentreEllipseMethod = inject("beam_centre"),
     oav: OAVBeamCentreFile = inject("oav1"),
 ) -> MsgGenerator:
+    # Read image size from PVs and set the ROI box to the greater one.
     x_size = yield from bps.rd(oav.grid_snapshot.x_size)
     y_size = yield from bps.rd(oav.grid_snapshot.y_size)
     roi_size = max(x_size, y_size)
-    yield from bps.abs_set(
-        beam_centre.roi_box_size, roi_size
-    )  # set it to something much bigger than needed so it will get fullscreen size
+    yield from bps.abs_set(beam_centre.roi_box_size, roi_size)
     LOGGER.info(f"Finding beam centre with roi_box_size set to {roi_size}")
     yield from bps.trigger(beam_centre)
