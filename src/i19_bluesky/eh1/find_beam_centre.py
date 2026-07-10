99c86e69afc836f6cc9b3bac7a69dfd67b3ba2a5
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2026-07-10 17:42:22 +0100

    Read image size from PV and use greater one for roi

--- src/i19_bluesky/eh1/find_beam_centre.py
+++ src/i19_bluesky/eh1/find_beam_centre.py
@@ -2,15 +2,20 @@ import bluesky.plan_stubs as bps
 from bluesky.utils import MsgGenerator
 from dodal.common import inject
 from dodal.devices.oav.beam_centre.beam_centre import CentreEllipseMethod
+from dodal.devices.oav.oav_detector import OAVBeamCentreFile
 
 from i19_bluesky.log import LOGGER
 
 
 def find_beam_centre_plan(
     beam_centre: CentreEllipseMethod = inject("beam_centre"),
+    oav: OAVBeamCentreFile = inject("oav1"),
 ) -> MsgGenerator:
+    x_size = yield from bps.rd(oav.grid_snapshot.x_size)
+    y_size = yield from bps.rd(oav.grid_snapshot.y_size)
+    roi_size = max(x_size, y_size)
     yield from bps.abs_set(
-        beam_centre.roi_box_size, 10000
+        beam_centre.roi_box_size, roi_size
     )  # set it to something much bigger than needed so it will get fullscreen size
-    LOGGER.info("Finding beam centre with roi_box_size set to 10000")
+    LOGGER.info(f"Finding beam centre with roi_box_size set to {roi_size}")
     yield from bps.trigger(beam_centre)
--- tests/unit_tests/eh1/test_find_beam_centre.py
+++ tests/unit_tests/eh1/test_find_beam_centre.py
@@ -4,6 +4,7 @@ import numpy as np
 import pytest
 from bluesky import RunEngine
 from dodal.devices.oav.beam_centre.beam_centre import CentreEllipseMethod
+from dodal.devices.oav.oav_detector import OAVBeamCentreFile
 from ophyd_async.core import init_devices, set_mock_value
 
 from i19_bluesky.eh1.find_beam_centre import find_beam_centre_plan
@@ -18,11 +19,33 @@ async def centre_device() -> CentreEllipseMethod:
     return centre_device
 
 
+@pytest.fixture
+async def oav() -> OAVBeamCentreFile:
+    oav_config = MagicMock()
+    async with init_devices(mock=True, connect=True):
+        oav = OAVBeamCentreFile("", config=oav_config, name="oav")
+    set_mock_value(oav.grid_snapshot.x_size, 1024)
+    set_mock_value(oav.grid_snapshot.y_size, 768)
+    return oav
+
+
+@pytest.mark.parametrize(
+    "image_size, expected_roi",
+    [([768, 1024], 1024), ([1024, 1024], 1024), ([1292, 964], 1292)],
+)
 @patch("i19_bluesky.eh1.find_beam_centre.bps.trigger")
 async def test_find_beam_centre_plan(
-    mock_trigger: MagicMock, centre_device: CentreEllipseMethod, RE: RunEngine
+    mock_trigger: MagicMock,
+    image_size: list[int],
+    expected_roi: int,
+    centre_device: CentreEllipseMethod,
+    oav: OAVBeamCentreFile,
+    RE: RunEngine,
 ):
-    RE(find_beam_centre_plan(centre_device))
+    set_mock_value(oav.grid_snapshot.x_size, image_size[0])
+    set_mock_value(oav.grid_snapshot.y_size, image_size[1])
 
-    assert await centre_device.roi_box_size.get_value() == 10000
+    RE(find_beam_centre_plan(centre_device, oav))
+
+    assert await centre_device.roi_box_size.get_value() == expected_roi
     mock_trigger.assert_called_once()
