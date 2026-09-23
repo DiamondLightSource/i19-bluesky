ff85b61cb2344da476db7b290a92344432e9be87
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2026-09-23 16:42:48 +0100

    Add docstring

--- src/i19_bluesky/plans/temperature_control_plans.py
+++ src/i19_bluesky/plans/temperature_control_plans.py
@@ -7,11 +7,18 @@ from i19_bluesky.log import LOGGER
 
 
 def run_temperature_ramp(
-    target_temp: float,
+    target_temp_in_k: float,
     ramp_rate: float = 360,
     cryostream: OxfordCryoStream = inject("cryostream"),
 ) -> MsgGenerator:
-    LOGGER.info(f"Start a temperature ramp to {target_temp}K, with rate {360}K/h")
+    """Trigger a temperature ramp on the cryostream.
+
+    Args:
+        target_temp_in_k (float): temperature to reach, in Kelvin.
+        ramp_rate (float): temperature ramp rate, in K/h.
+        cryostream: the cryostream device.
+    """
+    LOGGER.info(f"Start a temperature ramp to {target_temp_in_k}K, with rate {360}K/h")
     yield from bps.abs_set(cryostream.ramp_rate, ramp_rate, wait=True)
-    yield from bps.abs_set(cryostream.ramp_temp, target_temp, wait=True)
+    yield from bps.abs_set(cryostream.ramp_temp, target_temp_in_k, wait=True)
     yield from bps.trigger(cryostream.ramp)
